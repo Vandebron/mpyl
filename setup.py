b758262d7d0bacfb368084878161d6d6a4de1391
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-12 14:07:37 +0800

    Add pipfile and install
    branch:

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import toml
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+
+def get_install_requirements():
+    try:
+        with open('Pipfile', 'r') as fh:
+            pipfile = fh.read()
+        pipfile_toml = toml.loads(pipfile)
+    except FileNotFoundError:
+        return []
+    try:
+        required_packages = pipfile_toml['packages'].items()
+    except KeyError:
+        return []
+    return ["{0}{1}".format(pkg, ver) if ver != "*"
+            else pkg for pkg, ver in required_packages]
+
+
+setup(
+    name="pympl",
+    version="0.0.1",
+    description="A python rewrite of mpl-modules",
+    install_requires=get_install_requirements(),
+    packages=find_packages()
+)
