3db933d0511fa096aefea4c15772dbf2b7cd7a74
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-20 14:17:32 +0800

    [ARC-488] Assert commit changes are subset of branch
    branch: feature/ARC-488-pympl-prototype

--- tests/stages/test_discovery.py
+++ tests/stages/test_discovery.py
@@ -1,12 +1,18 @@
 import unittest
 
-from pympl.stages.discovery import find_invalidated_projects
+from pympl.stages.discovery import find_invalidated_projects, find_changes_in_branch, find_changes_in_commit
 
 
 class TestDiscovery(unittest.TestCase):
     def test_find_invalidated_projects(self):
         invalidated = find_invalidated_projects({'tests/projects/service/file.scala', 'tests/some_file.txt'})
         self.assertEqual(invalidated, {'tests/projects/service/'})
+
+    def test_changes_in_commit_should_be_in_branch(self):
+        changes_in_branch = find_changes_in_branch()
+        changes_in_commit = find_changes_in_commit()
+
+        self.assertTrue(changes_in_commit.issubset(changes_in_branch))
 
 
 if __name__ == '__main__':
