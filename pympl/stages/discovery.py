b93f36fe96da61b17bdb5e9acadf2fb345af6201
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-20 15:05:29 +0800

    [ARC-488] Find projects for stage
    branch: feature/ARC-488-pympl-prototype

--- pympl/stages/discovery.py
+++ pympl/stages/discovery.py
@@ -1,5 +1,6 @@
 from pympl.project import Project
 from pympl.repo import Repository
+from pympl.stages import Stage
 
 
 def find_invalidated_projects(repository: Repository, change_set: set[str]):
@@ -7,3 +8,14 @@ def find_invalidated_projects(repository: Repository, change_set: set[str]):
     projects_invalidated = set(
         filter(lambda p: next(filter(lambda c: c.startswith(p), change_set), None), project_paths))
     return projects_invalidated
+
+
+def find_projects_for_stage(repository: Repository, stage: Stage) -> set[str]:
+    change_set = find_change_set_for_stage(repository, stage)
+    return find_invalidated_projects(repository, change_set)
+
+
+def find_change_set_for_stage(repository: Repository, stage: Stage) -> set[str]:
+    if stage is Stage.BUILD or stage is Stage.TEST:
+        return repository.changes_in_commit()
+    return repository.changes_in_branch()
