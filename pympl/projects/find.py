1f01430538f9f3199d15c41adbb85ee5e15a7891
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-20 13:16:59 +0800

    [ARC-488] Add test for projects load
    branch: feature/ARC-488-pympl-prototype

--- pympl/projects/find.py
+++ pympl/projects/find.py
@@ -2,13 +2,13 @@ from git import Git
 from pympl.project import Project, load_project
 
 
-def initialized_git() -> Git:
-    return Git(Git().rev_parse('--show-toplevel'))
+def repo_root() -> str:
+    return Git().rev_parse('--show-toplevel')
 
 
 def find_projects() -> list[str]:
-    return initialized_git().ls_files(f'**/{Project.project_yaml_path()}').splitlines()
+    return Git(repo_root()).ls_files(f'**/{Project.project_yaml_path()}').splitlines()
 
 
 def load_projects(paths: list[str]) -> list[Project]:
-    return list(map(lambda p: load_project(p, False), paths))
+    return list(map(lambda p: load_project(f'{repo_root()}/{p}', False), paths))
