92f23d394ad9ead786de8d34a94ffbe738e0677e
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-20 14:17:32 +0800

    [ARC-488] Assert commit changes are subset of branch
    branch: feature/ARC-488-pympl-prototype

--- pympl/stages/discovery.py
+++ pympl/stages/discovery.py
@@ -1,6 +1,7 @@
-from pympl.project import Project
+from git import Repo
 
-from pympl.projects.find import find_projects
+from pympl.project import Project
+from pympl.projects.find import find_projects, repo_root
 
 
 def find_invalidated_projects(change_set: set[str]):
@@ -8,3 +9,13 @@ def find_invalidated_projects(change_set: set[str]):
     projects_invalidated = set(
         filter(lambda p: next(filter(lambda c: c.startswith(p), change_set), None), project_paths))
     return projects_invalidated
+
+
+def find_changes_in_branch() -> set[str]:
+    repo = Repo(repo_root())
+    return set(repo.git.diff('main', name_only=True).splitlines())
+
+
+def find_changes_in_commit() -> set[str]:
+    repo = Repo(repo_root())
+    return set(repo.git.diff(None, name_only=True).splitlines())
