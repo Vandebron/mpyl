57bec70cb9224b0f948873a29a95b5791131d094
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-20 14:39:47 +0800

    [ARC-488] Move repo related functionality to own class
    that can be initialized with config
    
    branch: feature/ARC-488-pympl-prototype

---
+++ tests/projects/repo/test_repo.py
@@ -0,0 +1,17 @@
+import unittest
+
+from pympl.repo import Repository
+
+
+class RepoTestCase(unittest.TestCase):
+
+    def test_changes_in_commit_should_be_in_branch(self):
+        repo = Repository('main')
+        changes_in_branch = repo.changes_in_branch()
+        changes_in_commit = repo.changes_in_commit()
+
+        self.assertTrue(changes_in_commit.issubset(changes_in_branch))
+
+
+if __name__ == '__main__':
+    unittest.main()
