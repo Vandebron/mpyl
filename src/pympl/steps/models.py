e9caa838d441b94bc8cdac1b512f4672884a618f
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-26 20:52:54 +0800

    [ARC-488] Add build properties
    branch: feature/ARC-488-docker-build-step

--- src/pympl/steps/models.py
+++ src/pympl/steps/models.py
@@ -1,12 +1,33 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from ..project import Project
 from ..stage import Stage
+from ..target import Target
+
+
+@dataclass(frozen=True)
+class VersioningProperties:
+    pr_number: Optional[str]
+    tag: Optional[str]
+
+
+@dataclass(frozen=True)
+class BuildProperties:
+    build_id: str
+    target: Target
+    git: VersioningProperties
 
 
 @dataclass(frozen=True)
 class Input:
     project: Project
+    build_properties: BuildProperties
+
+    def docker_image_tag(self):
+        git = self.build_properties.git
+        tag = f"pr-{git.pr_number}" if git.pr_number else git.tag
+        return f"{self.project.name.lower()}:{tag}"
 
 
 @dataclass(frozen=True)
