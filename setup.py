772e64c9b364e2bf2db2d8e6c93e875062490fdb
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-11-28 15:45:21 +0800

    Create new release
    branch:

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_install_requirements():
 
 setup(
     name="pympl",
-    version="0.0.8",
+    version="0.0.9",
     description="A python rewrite of mpl-modules",
     long_description=readme,
     long_description_content_type="text/markdown",
