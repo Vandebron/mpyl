90e869ee6210887ff31a886f475cbe27cf8412b3
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-12-10 21:43:11 +0800

    [ARC-488] Execute step only if artifact not yet present
    branch: feature/ARC-488-step-outputs-inputs

--- src/pympl/project.py
+++ src/pympl/project.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from typing import Optional, TypeVar, Dict, Any
 
 import jsonschema
-import yaml
 from mypy.checker import Generic
+from ruamel.yaml import YAML
 
 from .stage import Stage
 from .target import Target
@@ -240,7 +240,8 @@ class Project:
 def load_project(root_dir, project_path: str, strict: bool = True) -> Project:
     with open(f'{root_dir}/{project_path}') as f:
         try:
-            yaml_values = yaml.load(f, Loader=yaml.FullLoader)
+            yaml = YAML()
+            yaml_values = yaml.load(f)
             template = pkgutil.get_data(__name__, "schema/project.schema.json")
             if strict and template:
                 schema = json.loads(template.decode('utf-8'))
