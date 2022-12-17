a299753939e4ac3d0557bd2189f2a787ffd616b8
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-12-17 15:07:01 +0800

    [ARC-488] Simplify example project
    branch: feature/ARC-488-k8s-deploy

--- tests/test_project.py
+++ tests/test_project.py
@@ -12,36 +12,25 @@ class TestMplSchema(unittest.TestCase):
 
     def test_schema_load(self):
         project = load_project("", str(self.resource_path / "test_project.yml"))
-        self.assertEqual(project.name, 'nginx')
-        self.assertEqual(project.maintainer, ['Marketplace', 'EV Home'])
+        self.assertEqual(project.name, 'dockertest')
+        self.assertEqual(project.maintainer, ['Marketplace', 'Energy Trading'])
         envs = project.deployment.properties.env
 
-        simple_env = [x for x in envs if x.key == 'ENV_NGINX_CONF_FOLDER'].pop()
-        self.assertEqual(simple_env.key, 'ENV_NGINX_CONF_FOLDER')
-        self.assertEqual(simple_env.get_value(Target.ACCEPTANCE), '/etc/nginx')
-        self.assertEqual(simple_env.get_value(Target.PRODUCTION), '/etc/nginx')
+        simple_env = [x for x in envs if x.key == 'SOME_ENV'].pop()
+        self.assertEqual(simple_env.key, 'SOME_ENV')
+        self.assertEqual(simple_env.get_value(Target.ACCEPTANCE), 'Acceptance')
+        self.assertEqual(simple_env.get_value(Target.PRODUCTION), 'Production')
 
-        multi_env = [x for x in envs if x.key == 'ENV_SERVER_NAME'].pop()
-        self.assertEqual(multi_env.key, 'ENV_SERVER_NAME')
-        self.assertEqual(multi_env.get_value(Target.PULL_REQUEST), 'test-{PR-NUMBER}.test.vdbinfra.nl')
-        self.assertEqual(multi_env.get_value(Target.PULL_REQUEST_BASE), 'test.vdbinfra.nl')
-        self.assertEqual(multi_env.get_value(Target.ACCEPTANCE), 'acceptance.vdbinfra.nl')
-        self.assertEqual(multi_env.get_value(Target.PRODUCTION), 'vandebron.nl')
-
-        self.assertEqual(project.dependencies.build, {'apps/', 'server/', 'nginx-gateway/', 'client'})
+        self.assertEqual(project.dependencies.build, {'test/docker/'})
         self.assertEqual(project.dependencies.test, set())
 
-        self.assertEqual(project.deployment.kubernetes.portMappings, {9090: 80, 9091: 84})
+        self.assertEqual(project.deployment.kubernetes.portMappings, {8080: 8080})
         self.assertEqual(project.deployment.kubernetes.livenessProbe.path.get_value(Target.ACCEPTANCE), '/health')
         self.assertEqual(project.deployment.kubernetes.metrics.enabled, False)
 
         host = project.deployment.traefik.hosts[0]
-        self.assertEqual(host.host.get_value(Target.PULL_REQUEST_BASE),
-                         'HostRegexp(`{subdomain:(bewindvoering|ev|mijn|nom|vrienden|werkenbij|www|blog){1}}'
-                         '.test.vdbinfra.nl`, `test.vdbinfra.nl`)')
-        self.assertEqual(host.tls.get_value(Target.PULL_REQUEST_BASE), 'le-production-frontend-wildcard-cert')
-        self.assertEqual(host.whitelists.get_value(Target.ACCEPTANCE),
-                         ['NAT-Gateway-Service', 'Salesforce', 'K8s-Acceptance'])
+        self.assertEqual(host.host.get_value(Target.PULL_REQUEST_BASE), 'Host(`payments.test.vdbinfra.nl`)')
+        self.assertEqual(host.tls.get_value(Target.PULL_REQUEST_BASE), 'le-custom-prod-wildcard-cert')
 
     def test_schema_load_validation(self):
         with self.assertRaises(ValidationError):
