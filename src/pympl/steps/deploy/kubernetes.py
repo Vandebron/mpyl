1e4194f103ece0f71fd2d4794c68d8f757697144
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-12-18 15:49:50 +0800

    [ARC-488] Stub kubernetes helm deploy
    branch: feature/ARC-488-k8s-deploy

--- src/pympl/steps/deploy/kubernetes.py
+++ src/pympl/steps/deploy/kubernetes.py
@@ -1,9 +1,12 @@
 from logging import Logger
+from pathlib import Path
 
 from .k8s.service import ServiceDeployment, to_yaml
 from ..models import Meta, Input, Output, ArtifactType
 from ..step import Step
 from ...stage import Stage
+from pyhelm.chartbuilder import ChartBuilder
+from pyhelm.tiller import Tiller
 
 
 class DeployKubernetes(Step):
@@ -20,6 +23,18 @@ class DeployKubernetes(Step):
         self._logger.info(f"Deploying project {step_input.project.name}")
 
         dep = ServiceDeployment(step_input)
-        self._logger.info(to_yaml(dep))
+
+        templates = dep.to_chart()
+
+        chart_path = Path(step_input.project.target_path) / "chart" / "templates"
+        Path(chart_path).mkdir(parents=True, exist_ok=True)
+
+        for k, v in templates.items():
+            with open(chart_path / str(k), mode='w+') as file:
+                file.write(v)
+
+        chart = ChartBuilder({"name": step_input.project.name, "source": {"type": "directory", "location": chart_path}})
+
+        self._logger.info(chart.dump())
 
         return Output(success=True, message=f"Deployed project {step_input.project.name}")
