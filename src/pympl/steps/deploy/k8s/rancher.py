549ea905b8b8cc2ee2b045ad7c31fc524b20849c
ewaldschrader <ewaldschrader@vandebron.nl>
2023-01-26 15:29:44 +0100

    [ARC-532] Fixed pylint errors: string format
    Remove pylint errors
    
    branch: feature/ARC-532-introduce-pylint

--- src/pympl/steps/deploy/k8s/rancher.py
+++ src/pympl/steps/deploy/k8s/rancher.py
@@ -11,7 +11,7 @@ class ClusterConfig:
 
 
 def cluster_config(target: Target):
-    if target == Target.PULL_REQUEST or target == Target.PULL_REQUEST_BASE:
+    if target in {Target.PULL_REQUEST, Target.PULL_REQUEST_BASE}:
         return ClusterConfig(cluster_id='c-z8wzm', project_id='p-k9l47', cluster_env="test")
     if target == Target.ACCEPTANCE:
         return ClusterConfig(cluster_id='c-6mkzg', project_id='p-ckqxz', cluster_env="acce")
