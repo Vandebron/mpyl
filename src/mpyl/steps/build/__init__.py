e939e18e65107abdc2edc69a4a4fae56fa6d1397
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-10 19:33:51 +0800

    [ARC-568] Make docker settings configurable
    branch: feature/ARC-568_create_jenkinsfile-alternative

--- src/mpyl/steps/build/__init__.py
+++ src/mpyl/steps/build/__init__.py
@@ -5,6 +5,9 @@ class DockerConfig:
     host_name: str
     user_name: str
     password: str
+    root_folder: str
+    build_target: str
+    docker_file_name: str
 
     def __init__(self, config: Dict):
         try:
@@ -12,5 +15,10 @@ class DockerConfig:
             self.host_name = registry['host_name']
             self.user_name = registry['user_name']
             self.password = registry['password']
+            build: dict = config['docker']['build']
+            self.root_folder = build['root_folder']
+            self.build_target = build['build_target']
+            self.docker_file_name = build['docker_file_name']
+
         except KeyError as exc:
             raise KeyError(f'Docker config could not be loaded from {config}') from exc
