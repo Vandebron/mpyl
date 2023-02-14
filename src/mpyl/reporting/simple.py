e16ffd30719e1a08fe713de53972fd35891f7a5e
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-14 15:06:24 +0800

    [ARC-525] Add basic junit test suite dump
    branch: feature/ARC-525-report-results

--- src/mpyl/reporting/simple.py
+++ src/mpyl/reporting/simple.py
@@ -1,5 +1,9 @@
+import os
+
+from ..steps.models import Artifact, ArtifactType
 from ...mpyl import Stage
 from ...mpyl.steps.run import RunResult
+from junitparser import JUnitXml, TestSuite, TestCase
 
 
 def to_string(run_result: RunResult) -> str:
@@ -11,3 +15,20 @@ def to_string(run_result: RunResult) -> str:
             for res in run_results:
                 result += f"{res.timestamp} - {res.project.name} - {res.stage} - success: {res.output.success} \n"
     return result
+
+
+def to_test_report(artifact: Artifact):
+    if artifact.artifact_type != ArtifactType.JUNIT_TESTS:
+        raise ValueError(f'Artifact {artifact} should be of type {ArtifactType.JUNIT_TESTS.name}')
+    junit_result_path = artifact.spec['test_output_path']
+
+    test_result: str = ""
+    xml = JUnitXml()
+    for file_name in [fn for fn in os.listdir(junit_result_path) if fn.endswith('.xml')]:
+        xml += JUnitXml.fromfile(junit_result_path / file_name)
+
+    for suite in (TestSuite.fromelem(s) for s in xml):
+        test_result += f"Suite {suite.name}\n"
+        for case in suite:
+            test_result += f"Case {case.name} \n"
+    return test_result
