5e9973929fca8803e238bfbcf26ef697e530b640
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-14 11:32:54 +0800

    [ARC-525] Introduce simple (text) result reporter
    branch: feature/ARC-525-report-results

--- tests/test_resources/test_data.py
+++ tests/test_resources/test_data.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pyaml_env import parse_config
 
 from src.mpyl import Target
@@ -11,5 +13,14 @@ config_values = parse_config(resource_path / "config.yml")
 RUN_PROPERTIES = RunProperties("id", Target.PULL_REQUEST,
                                VersioningProperties("2ad3293a7675d08bc037ef0846ef55897f38ec8f", "1234", None),
                                config_values)
-
 TEST_PROJECT = load_project(resource_path, "test_project.yml", False)
+
+
+def assert_roundtrip(file_path: Path, expected_contents: str, overwrite: bool = False):
+    if overwrite:
+        with open(file_path, 'w+', encoding='utf-8') as file:
+            file.write(expected_contents)
+            assert not overwrite, "Should not commit with overwrite"
+
+    with open(file_path, encoding='utf-8') as file:
+        assert file.read() == expected_contents
