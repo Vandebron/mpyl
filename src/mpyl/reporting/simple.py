64af179899cc5e300f91a8d7c087efb9d269c0f0
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-14 15:33:33 +0800

    [ARC-525] Sum all totals in the test run
    branch: feature/ARC-525-report-results

--- src/mpyl/reporting/simple.py
+++ src/mpyl/reporting/simple.py
@@ -1,4 +1,6 @@
 import os
+from dataclasses import dataclass
+from functools import reduce
 
 from ..steps.models import Artifact, ArtifactType
 from ...mpyl import Stage
@@ -17,18 +19,39 @@ def to_string(run_result: RunResult) -> str:
     return result
 
 
-def to_test_report(artifact: Artifact):
+@dataclass(frozen=True)
+class TestRunSummary:
+    tests: int
+    failures: int
+    errors: int
+    skipped: int
+
+
+def sum_suites(suites: list[TestSuite]) -> TestRunSummary:
+    return TestRunSummary(tests=sum(s.tests for s in suites), failures=sum(s.failures for s in suites),
+                          errors=sum(s.failures for s in suites), skipped=sum(s.skipped for s in suites))
+
+
+def to_test_suites(artifact: Artifact):
     if artifact.artifact_type != ArtifactType.JUNIT_TESTS:
         raise ValueError(f'Artifact {artifact} should be of type {ArtifactType.JUNIT_TESTS.name}')
     junit_result_path = artifact.spec['test_output_path']
 
-    test_result: str = ""
     xml = JUnitXml()
     for file_name in [fn for fn in os.listdir(junit_result_path) if fn.endswith('.xml')]:
         xml += JUnitXml.fromfile(junit_result_path / file_name)
 
-    for suite in (TestSuite.fromelem(s) for s in xml):
+    return [TestSuite.fromelem(s) for s in xml]
+
+
+def to_test_report(artifact: Artifact):
+    test_result = ""
+    suites = to_test_suites(artifact)
+    total_tests = sum_suites(suites)
+    test_result += f"{total_tests} \n\n"
+    for suite in suites:
         test_result += f"Suite {suite.name}\n"
         for case in suite:
             test_result += f"Case {case.name} \n"
+
     return test_result
