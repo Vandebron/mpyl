9b2d0d61e8572312d9e9b8dba79146af3e85b071
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-16 14:40:29 +0800

    [ARC-525] Report build results to github
    branch: feature/ARC-525-report-results

--- mpyl-test-runner.py
+++ mpyl-test-runner.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
-import time
 
 from dagster import job, op, DynamicOut, DynamicOutput, get_dagster_logger, Output, Failure
 from pyaml_env import parse_config
 
+from src.mpyl import Stage
 from src.mpyl.project import load_project, Project
 from src.mpyl.repo import Repository, RepoConfig
-from src.mpyl import Stage
-from src.mpyl.steps.models import RunProperties, Output as MplOutput
-from src.mpyl.steps.steps import Steps
+from src.mpyl.reporting.simple import to_string
+from src.mpyl.reporting.targets.github import GithubReport
+from src.mpyl.steps.models import RunProperties
+from src.mpyl.steps.run import RunResult
+from src.mpyl.steps.steps import Steps, StepResult
 
 
 @dataclass
@@ -17,15 +19,20 @@ class StepParam:
     project: Project
 
 
-def execute_step(proj: Project, stage: Stage) -> MplOutput:
+def execute_step(proj: Project, stage: Stage) -> StepResult:
     config = parse_config("config.yml")
     properties = parse_config("run_properties.yml")
     run_properties = RunProperties.from_configuration(run_properties=properties, config=config)
-    executor = Steps(get_dagster_logger(), run_properties)
-    step_output = executor.execute(stage, proj).output
-    if not step_output.success:
-        raise Failure(description=step_output.message)
-    return step_output
+    dagster_logger = get_dagster_logger()
+    executor = Steps(dagster_logger, run_properties)
+    step_result = executor.execute(stage, proj)
+    # message = f'Executed {stage.name} for {proj.name}'
+    # dagster_logger.info(message)
+    # output = MplOutput(success=True, message=message)
+    # step_result = StepResult(stage=stage, project=proj, output=output)
+    if not step_result.output.success:
+        raise Failure(description=step_result.output.message)
+    return step_result
 
 
 @op
@@ -43,11 +50,30 @@ def deploy_project(project: Project) -> Output:
     return Output(execute_step(project, Stage.DEPLOY))
 
 
-@op(out=DynamicOut())
-def deploy_projects(projects: list[Project], outputs: list[MplOutput]):
+@op
+def deploy_projects(projects: list[Project], outputs: list[StepResult]) -> Output[list[StepResult]]:
+    res = []
     for proj in projects:
-        result = execute_step(proj, Stage.DEPLOY)
-        yield DynamicOutput(result, mapping_key=f"deployed_{proj.name}")
+        res.append(execute_step(proj, Stage.DEPLOY))
+    return Output(res)
+
+
+@op
+def report_results(build_results: list[StepResult], deploy_results: list[StepResult]) -> bool:
+    config = parse_config("config.yml")
+
+    properties = RunProperties.from_configuration(parse_config("run_properties.yml"), config)
+
+    run_result = RunResult(properties)
+    run_result.extend(build_results)
+    run_result.extend(deploy_results)
+
+    logger = get_dagster_logger()
+    logger.info(to_string(run_result))
+
+    report = GithubReport(config)
+    report.send_report(run_result)
+    return True
 
 
 @op(out=DynamicOut())
@@ -63,12 +89,14 @@ def find_projects() -> list[DynamicOutput[Project]]:
 def run_build():
     projects = find_projects()
     build_results = projects.map(build_project)
-    deploy_projects(
+    deploy_results = deploy_projects(
         projects=projects.collect(),
         outputs=build_results.collect()
     )
+    report_results(build_results=build_results.collect(), deploy_results=deploy_results)
 
 
 if __name__ == "__main__":
+
     result = run_build.execute_in_process()
     print(f"Result: {result.success}")
