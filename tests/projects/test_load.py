c12ebad8a73844ef1be2a8beb8efb86d3fb5be63
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-16 12:14:26 +0800

    [ARC-525] Add stub github reporter
    branch: feature/ARC-525-report-results

--- tests/projects/test_load.py
+++ tests/projects/test_load.py
@@ -4,7 +4,7 @@ import unittest
 import pytest
 
 from src.mpyl.projects.find import load_projects
-from src.mpyl.repo import Repository, RepoConfig
+from tests.test_resources import test_data
 
 
 class ProjectLoadTestCase:
@@ -12,7 +12,7 @@ class ProjectLoadTestCase:
     @pytest.mark.skipif(condition="GITHUB_JOB" in os.environ,
                         reason="fatal: detected dubious ownership in repository at '/github/workspace'")
     def test_load_all_projects(self):
-        repo = Repository(RepoConfig({'cvs': {'git': {'mainBranch': 'main'}}}))
+        repo = test_data.TEST_REPO
         projects = load_projects(repo.root_dir(), repo.find_projects())
 
         assert len(projects) == 2
