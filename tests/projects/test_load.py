3f57190065cd1f66e0aec368ac2e79567edd74b8
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-16 19:04:34 +0800

    [ARC-525] Remove remaining references to unittest
    branch: feature/ARC-525-report-results

--- tests/projects/test_load.py
+++ tests/projects/test_load.py
@@ -1,5 +1,4 @@
 import os
-import unittest
 
 import pytest
 
@@ -16,7 +15,3 @@ class ProjectLoadTestCase:
         projects = load_projects(repo.root_dir(), repo.find_projects())
 
         assert len(projects) == 2
-
-
-if __name__ == '__main__':
-    unittest.main()
