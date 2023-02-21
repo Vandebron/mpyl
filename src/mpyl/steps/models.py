a2fbe488959979ebfbd636245c0af0711211748f
ewaldschrader <117187912+ewaldschrader@users.noreply.github.com>
2023-02-21 21:51:57 +0100

    Update models.py

--- src/mpyl/steps/models.py
+++ src/mpyl/steps/models.py
@@ -83,7 +83,7 @@ class Input:
     project: Project
     run_properties: RunProperties
     required_artifact: Optional[Artifact] = None
-    dry_run: bool = True
+    dry_run: bool = False
 
     def docker_image_tag(self):
         git = self.run_properties.versioning
