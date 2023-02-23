e50b7e769a8578b570745c7cf3cb746d479bdc0b
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-23 14:37:22 +0800

    [ARC-576] Feed results of previous step into next one
    so that a higher level of paralellism can be reached
    
    branch: feature/ARC-576-add-docker-test-step

--- src/mpyl/utilities/junit/__init__.py
+++ src/mpyl/utilities/junit/__init__.py
@@ -2,6 +2,7 @@
 
 import os
 from dataclasses import dataclass
+from pathlib import Path
 
 from junitparser import JUnitXml, TestSuite
 
@@ -22,14 +23,14 @@ class TestRunSummary:
         return self.errors == 0 and self.failures == 0
 
 
-def to_test_suites(artifact: Artifact):
+def to_test_suites(artifact: Artifact) -> list[TestSuite]:
     if artifact.artifact_type != ArtifactType.JUNIT_TESTS:
         raise ValueError(f'Artifact {artifact} should be of type {ArtifactType.JUNIT_TESTS}')
     junit_result_path = artifact.spec[TEST_OUTPUT_PATH_KEY]
 
     xml = JUnitXml()
     for file_name in [fn for fn in os.listdir(junit_result_path) if fn.endswith('.xml')]:
-        xml += JUnitXml.fromfile(junit_result_path / file_name)
+        xml += JUnitXml.fromfile(Path(junit_result_path, file_name))
 
     suites = [TestSuite.fromelem(s) for s in xml]
     return sorted(suites, key=lambda s: s.time)
