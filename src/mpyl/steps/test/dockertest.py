26ce667d0a2dbd3ed483b874a2c9b2a41a16c358
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-23 16:08:06 +0800

    [ARC-576] Replace docker client with 'python on whales'
    because it supports the (buildx) buildkit builder.
    And has a better designed API.
    
    branch: feature/ARC-576-add-docker-test-step

--- src/mpyl/steps/test/dockertest.py
+++ src/mpyl/steps/test/dockertest.py
@@ -1,15 +1,14 @@
 """ Step that tests the docker image from the target `tester` in Dockerfile-mpl. """
 import shutil
-import tarfile
 from logging import Logger
 from pathlib import Path
 
-from docker import APIClient
+from python_on_whales import docker
 
 from ..models import Meta, Input, Output, ArtifactType, input_to_artifact, Artifact
 from ..step import Step
-from ...utilities.docker import DockerConfig, build, docker_image_tag, docker_file_path
 from ...project import Stage, Project
+from ...utilities.docker import DockerConfig, build, docker_image_tag, docker_file_path
 from ...utilities.junit import to_test_suites, sum_suites, TEST_OUTPUT_PATH_KEY
 
 
@@ -32,13 +31,11 @@ class TestDocker(Step):
         project = step_input.project
         dockerfile = docker_file_path(project=project, docker_config=docker_config)
 
-        client: APIClient = APIClient()
-
-        success = build(logger=self._logger, docker_client=client, root_path=docker_config.root_folder,
+        success = build(logger=self._logger, root_path=docker_config.root_folder,
                         file_path=dockerfile, image_tag=tag, target=test_target)
 
         if success:
-            artifact = self.extract_test_results(self._logger, client, project, tag, step_input)
+            artifact = self.extract_test_results(project, tag, step_input)
 
             suite = to_test_suites(artifact)
             summary = sum_suites(suite)
@@ -51,21 +48,13 @@ class TestDocker(Step):
                       produced_artifact=None)
 
     @staticmethod
-    def extract_test_results(logger: Logger, client: APIClient, project: Project, tag, step_input: Input) -> Artifact:
+    def extract_test_results(project: Project, tag, step_input: Input) -> Artifact:
         test_result_path = Path(project.target_path, "test_results")
         shutil.rmtree(test_result_path, ignore_errors=True)
         Path(test_result_path).mkdir(parents=True, exist_ok=True)
-        tar_path: Path = test_result_path / 'test_results.tar'
 
-        container_id = client.create_container(tag)
-        bits, stat = client.get_archive(container_id, project.test_report_path)
-        logger.debug(f"Stats for {tar_path}: {stat}")
-
-        with open(tar_path, mode='wb+') as file:
-            for chunk in bits:
-                file.write(chunk)
-        with tarfile.open(tar_path) as tar_file:
-            tar_file.extractall(path=test_result_path)
+        container_id = docker.create(tag).id
+        docker.copy(f'{container_id}:/{project.test_report_path}/.', test_result_path)
 
         return input_to_artifact(artifact_type=ArtifactType.JUNIT_TESTS, step_input=step_input,
-                                 spec={TEST_OUTPUT_PATH_KEY: f'{test_result_path}/test-reports'})
+                                 spec={TEST_OUTPUT_PATH_KEY: f'{test_result_path}'})
