3c9d89f7a72fe41a9a185e01e71ecdd6d351aa3c
koeves <daniel.koves@icloud.com>
2023-02-23 17:00:32 +0100

    [ARC-522] Add sbt build step
    branch: feature/ARC-522_add_sbt_build_step

---
+++ src/mpyl/utilities/sbt/__init__.py
@@ -0,0 +1,10 @@
+"""SBT config"""
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SbtConfig:
+    java_opts_file_name: str = '.jvmopts'
+    sbt_opts: str = '-Xmx4G -Xms4G -XX:+UseG1GC -XX:+CMSClassUnloadingEnabled -Xss2M -Duser.timezone=GMT ' \
+                    '-Dsbt.log.noformat=true '
+    sbt_command: str = 'DOCKER_BUILDKIT=1 sbtn '
