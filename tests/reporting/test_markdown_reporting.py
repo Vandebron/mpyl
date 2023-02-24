50db69f66c3f9dc1b8ce526840df90a387453119
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-02-24 11:06:21 +0800

    [ARC-525] Add markdown reporters
    branch: feature/ARC-525-report-build-progress

---
+++ tests/reporting/test_markdown_reporting.py
@@ -0,0 +1,19 @@
+from src.mpyl.reporting.markdown import summary_to_markdown, run_result_to_markdown
+from src.mpyl.utilities.junit import TestRunSummary
+from tests import root_test_path
+from tests.reporting import create_test_result
+from tests.test_resources.test_data import assert_roundtrip
+
+
+class TestMarkdownReporting:
+    test_resource_path = root_test_path / "reporting" / "test_resources"
+
+    def test_should_print_results_as_string(self):
+        run_result = create_test_result()
+        simple_report = run_result_to_markdown(run_result)
+        assert_roundtrip(self.test_resource_path / "markdown_run.md", simple_report)
+
+    def test_should_convert_test_summary_to_markdown(self):
+        summary = TestRunSummary(tests=20, failures=2, errors=1, skipped=0)
+        test_report = summary_to_markdown(summary)
+        assert_roundtrip(self.test_resource_path / "test_run_summary.md", test_report)
---
+++ tests/reporting/test_simple_reporting.py
@@ -0,0 +1,22 @@
+from src.mpyl.reporting.simple import to_string, to_test_report
+from src.mpyl.steps.models import Artifact, ArtifactType
+from src.mpyl.utilities.junit import TEST_OUTPUT_PATH_KEY
+from tests import root_test_path
+from tests.reporting import create_test_result
+from tests.test_resources.test_data import assert_roundtrip
+
+
+class TestReporting:
+    test_resource_path = root_test_path / "reporting" / "test_resources"
+
+    def test_should_print_results_as_string(self):
+        run_result = create_test_result()
+        simple_report = to_string(run_result)
+        assert_roundtrip(self.test_resource_path / "simple_run.txt", simple_report)
+
+    def test_should_convert_test_report_to_string(self):
+        test_artifact = Artifact(artifact_type=ArtifactType.JUNIT_TESTS, revision='revision',
+                                 producing_step='Docker Test',
+                                 spec={TEST_OUTPUT_PATH_KEY: self.test_resource_path})
+        test_report = to_test_report(test_artifact)
+        assert_roundtrip(self.test_resource_path / "simple_test.txt", test_report, overwrite=False)
