cb6dcac5088b59bc7a0ceab77c53281bfe2cb194
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-01 10:16:45 +0800

    [ARC-522] Add example sbt project
    branch: feature/ARC-522_add_sbt_build_step

--- src/mpyl/utilities/sbt/__init__.py
+++ src/mpyl/utilities/sbt/__init__.py
@@ -7,4 +7,4 @@ class SbtConfig:
     java_opts_file_name: str = '.jvmopts'
     sbt_opts: str = '-Xmx4G -Xms4G -XX:+UseG1GC -XX:+CMSClassUnloadingEnabled -Xss2M -Duser.timezone=GMT ' \
                     '-Dsbt.log.noformat=true '
-    sbt_command: str = 'DOCKER_BUILDKIT=1 sbt --client'
+    sbt_command: str = 'DOCKER_BUILDKIT=1 sbt'
