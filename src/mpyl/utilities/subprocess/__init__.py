7815ad5ae497c71474bfc58b870e317ba1c89ffe
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-01 12:07:15 +0800

    [ARC-522] Use Popen instead of subprocess.run
    because it seems to be give a result that is most similar to running
    from the terminal.
    The pipe parameters to `subprocess.run` link to tty and give less
    control about the output stream
    
    branch: feature/ARC-522_add_sbt_build_step

--- src/mpyl/utilities/subprocess/__init__.py
+++ src/mpyl/utilities/subprocess/__init__.py
@@ -10,19 +10,15 @@ def custom_check_output(logger: Logger, command: list[str], pipe_output=True, sh
     command_argument = ' '.join(command)
     logger.info(f"Executing: '{command_argument}'")
     try:
-        output = subprocess.run(
-            command,
-            stdout=subprocess.PIPE if pipe_output else None,
-            stderr=subprocess.PIPE if pipe_output else None,
-            shell=shell,
-            check=True,
-        )
-        if output.returncode == 0:
-            if pipe_output:
-                message = output.stdout.decode()
-                logger.info(message)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
+        for line in iter(process.stdout.readline, ""):
+            if line:
+                logger.info(line.strip())
+            if process.poll() is not None:
+                break
+        exit_code = process.wait()
 
-            return Output(success=True, message='Subprocess executed successfully')
+        return Output(success=exit_code == 0, message='Subprocess executed successfully')
 
     except subprocess.CalledProcessError as exc:
         message = f"'{command_argument}': failed with return code: {exc.returncode} err: {exc.stderr.decode()}"
