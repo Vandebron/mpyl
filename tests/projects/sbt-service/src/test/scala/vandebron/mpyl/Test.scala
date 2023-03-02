2ad39a32866391c8ca09ce81e3d22b473cc970e3
koeves <daniel.koves@icloud.com>
2023-03-02 15:59:18 +0100

    [ARC-589] Add sample scala test
    branch: feature/ARC-589_sbt_test_step

---
+++ tests/projects/sbt-service/src/test/scala/vandebron/mpyl/Test.scala
@@ -0,0 +1,12 @@
+package nl.vandebron.api.mpyl
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class Test extends AnyWordSpec with Matchers {
+  "Test" should {
+    "succeed" in {
+      true shouldBe true
+    }
+  }
+}
