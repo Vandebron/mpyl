d6a7858ab75257042dca3e80f5b52c92332d8027
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-06 11:56:12 +0800

    [ARC-590] Add icons to message
    branch: featur/ARC-590-slack-reporter

--- src/mpyl/reporting/targets/slack.py
+++ src/mpyl/reporting/targets/slack.py
@@ -2,6 +2,7 @@
 permissions.
 """
 import re
+from dataclasses import dataclass
 from typing import Dict
 
 from slack_sdk import WebClient
@@ -9,6 +10,7 @@ from slack_sdk.models.blocks import HeaderBlock, SectionBlock, MarkdownTextObjec
 
 from . import Reporter
 from ..formatting.markdown import run_result_to_markdown
+from ...steps.models import RunProperties
 from ...steps.run import RunResult
 
 
@@ -26,28 +28,41 @@ def to_slack_markdown(markdown: str) -> str:
     return markdown
 
 
-class SlackReporter(Reporter):
+@dataclass(frozen=True)
+class SlackIcons:
+    success: str
+    failure: str
 
-    def __init__(self, config: Dict, channel: str):
+
+class SlackReporter(Reporter):
+    _icons: SlackIcons
+
+    def __init__(self, config: Dict, channel: str, title: str):
         slack_config = config.get('slack')
         if not slack_config:
             raise ValueError('slack config not set')
         self._client = WebClient(token=slack_config['botToken'])
         self._channel = channel
+        self._title = title
+        icons = config['icons']
+        self._icons = SlackIcons(success=icons['success'], failure=icons['failure'])
 
     def send_report(self, results: RunResult) -> None:
-        user = self._client.users_lookupByEmail(email='sam@vandebron.nl')
-        user_id = user['user']['id']
-
         build_props = results.run_properties
-        details = results.run_properties.details
 
-        context = self.compose_context(build_props, details)
+        icon = self._icons.success if results.is_success else self._icons.failure
+        context = self.compose_context(build_props, icon)
+
+        initiator = ''
+        if not results.is_success:
+            user = self._client.users_lookupByEmail(email=build_props.details.user_email)
+            user_id = user['user']['id']
+            initiator = f'<@{user_id}>' if user_id else ''
 
         text = to_slack_markdown(
-            run_result_to_markdown(results) + (f'<@{user_id}>' if user_id else ''))
+            run_result_to_markdown(results) + initiator)
 
-        blocks = [HeaderBlock(text="Master nightly pipeline build results"),
+        blocks = [HeaderBlock(text=self._title),
                   SectionBlock(text=MarkdownTextObject(text=text)),
                   ContextBlock(elements=[MarkdownTextObject(text=context)])
                   ]
@@ -55,7 +70,8 @@ class SlackReporter(Reporter):
         self._client.chat_postMessage(channel=self._channel, icon_emoji=':robot_face:', mrkdwn=True, blocks=blocks)
 
     @staticmethod
-    def compose_context(build_props, details) -> str:
-        return f"icon Build <{details.run_url}|{details.build_id.upper()}> and changes in " \
+    def compose_context(build_props: RunProperties, icon: str) -> str:
+        details = build_props.details
+        return f":{icon}: Build <{details.run_url}|{details.build_id.upper()}> and changes in " \
                f"<{details.change_url}|{build_props.versioning.identifier.upper()}> " \
-               f"started by _{build_props.details.user}_"
+               f"started by _{details.user}_"
