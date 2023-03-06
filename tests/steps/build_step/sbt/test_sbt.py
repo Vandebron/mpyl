651eb211214fc9010e11505935baa625a624d986
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-06 10:24:07 +0800

    [ARC-589] Use colored output
    branch: feature/ARC-589_sbt_test_step

--- tests/steps/build_step/sbt/test_sbt.py
+++ tests/steps/build_step/sbt/test_sbt.py
@@ -9,14 +9,7 @@ class TestBuildSbt:
     def test_sbt_command_should_be_properly_constructed(self):
         step_input = Input(get_project(), test_data.RUN_PROPERTIES, None)
         command = BuildSbt._construct_sbt_command(step_input, 'imagename:latest')
-        assert command == ['sbt',
-                           '-v',
-                           '-J-Xmx4G',
-                           '-J-Xms4G',
-                           '-J-XX:+UseG1GC',
-                           '-J-XX:+CMSClassUnloadingEnabled',
-                           '-J-Xss2M',
-                           '-Duser.timezone=GMT',
-                           '-Dsbt.log.noformat=true',
-                           'project dockertest; set docker / imageNames := '
-                           'Seq(ImageName("imagename:latest")); scalafmtCheckAll; docker']
+        assert ' '.join(command) == ('sbt -v -J-Xmx4G -J-Xms4G -J-XX:+UseG1GC -J-XX:+CMSClassUnloadingEnabled '
+                                     '-J-Xss2M -Duser.timezone=GMT -Djline.terminal=jline.UnixTerminal project '
+                                     'dockertest; set docker / imageNames := Seq(ImageName("imagename:latest")); '
+                                     'scalafmtCheckAll; docker')
