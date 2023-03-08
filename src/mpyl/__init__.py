b2da4c0a4b99b4b4a89675d1b7719e1ad67354e8
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-08 15:08:40 +0800

    [ARC-590] Move developer instructions to mainline
    branch: featur/ARC-590-slack-reporter

--- src/mpyl/__init__.py
+++ src/mpyl/__init__.py
@@ -1,11 +1,7 @@
 """
 .. include:: ../../README.md
 
-# How do I?
+.. include:: ../../README-usage.md
 
-## ..report the outcome of a pipeline run
-MPyL comes with built-in reporters for *Github*, *Jira* and *Slack*. See `mpyl.reporting.targets` how to configure
-them and for instructions on how to create your own reporter.
-## ..create a custom step
-See `mpyl.steps`.
+.. include:: ../../README-dev.md
 """
