3ce82d209f237ab61e0ebba7cf30bb4337e447ca
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-08 13:55:31 +0800

    [ARC-590] Only print java details when verbose param is set
    branch: featur/ARC-590-slack-reporter

--- src/mpyl/utilities/sbt/__init__.py
+++ src/mpyl/utilities/sbt/__init__.py
@@ -10,6 +10,7 @@ class SbtConfig:
     sbt_opts: str
     sbt_command: str
     test_with_coverage: bool
+    verbose: bool
 
     @staticmethod
     def from_config(config: Dict):
@@ -19,10 +20,14 @@ class SbtConfig:
         return SbtConfig(sbt_command=sbt_config['command'],
                          java_opts=sbt_config['javaOpts'],
                          sbt_opts=sbt_config['sbtOpts'],
-                         test_with_coverage=(str(sbt_config['testWithCoverage']).lower() == 'true'))
+                         test_with_coverage=(str(sbt_config['testWithCoverage']).lower() == 'true'),
+                         verbose=(str(sbt_config['verbose']).lower() == 'true')
+                         )
 
     def to_command(self):
-        cmd = [self.sbt_command, '-v']
+        cmd = [self.sbt_command]
+        if self.verbose:
+            cmd.append('-v')
         cmd.extend([f'-J{opt}' for opt in self.java_opts.split(' ')])
         cmd.extend([f'-D{opt}' for opt in self.sbt_opts.split(' ')])
         return cmd
