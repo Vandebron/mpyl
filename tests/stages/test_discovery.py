562424b9110025de040df54ffd909b90f8bba916
koeves <daniel.koves@icloud.com>
2023-03-09 16:02:37 +0100

    [ARC-524] Add todo to test
    branch: feature/ARC-524-K8s-deploy-job

--- tests/stages/test_discovery.py
+++ tests/stages/test_discovery.py
@@ -4,8 +4,8 @@ import pytest
 
 from src.mpyl.project import Stage
 from src.mpyl.projects.find import load_projects
-from src.mpyl.utilities.repo import History
 from src.mpyl.stages.discovery import find_invalidated_projects_for_stage
+from src.mpyl.utilities.repo import History
 from tests.test_resources import test_data
 
 
@@ -20,7 +20,8 @@ class TestDiscovery:
         assert len(
             find_invalidated_projects_for_stage(projects, Stage.BUILD, [History(0, "revision", touched_files)])) == 1
         assert len(
-            find_invalidated_projects_for_stage(projects, Stage.TEST, [History(0, "revision", touched_files)])) == 2
+            find_invalidated_projects_for_stage(projects, Stage.TEST, [
+                History(0, "revision", touched_files)])) == 2  # TODO: the tests say it's one, not sure what's changed
         assert len(
             find_invalidated_projects_for_stage(projects, Stage.DEPLOY, [History(0, "revision", touched_files)])) == 1
 
