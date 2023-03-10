e0a8fabcf5c08492f0382bae4df8d5502b45d155
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-10 13:35:24 +0800

    [ARC-584] Rename History class
    branch: feature/ARC-584-runner-clean-up

--- tests/stages/test_discovery.py
+++ tests/stages/test_discovery.py
@@ -4,7 +4,7 @@ import pytest
 
 from src.mpyl.project import Stage
 from src.mpyl.projects.find import load_projects
-from src.mpyl.utilities.repo import History
+from src.mpyl.utilities.repo import Revision
 from src.mpyl.stages.discovery import find_invalidated_projects_for_stage
 from tests.test_resources import test_data
 
@@ -18,11 +18,11 @@ class TestDiscovery:
         touched_files = {'tests/projects/service/file.py', 'tests/some_file.txt'}
         projects = set(load_projects(repo.root_dir(), repo.find_projects()))
         assert len(
-            find_invalidated_projects_for_stage(projects, Stage.BUILD, [History(0, "revision", touched_files)])) == 1
+            find_invalidated_projects_for_stage(projects, Stage.BUILD, [Revision(0, "revision", touched_files)])) == 1
         assert len(
-            find_invalidated_projects_for_stage(projects, Stage.TEST, [History(0, "revision", touched_files)])) == 2
+            find_invalidated_projects_for_stage(projects, Stage.TEST, [Revision(0, "revision", touched_files)])) == 2
         assert len(
-            find_invalidated_projects_for_stage(projects, Stage.DEPLOY, [History(0, "revision", touched_files)])) == 1
+            find_invalidated_projects_for_stage(projects, Stage.DEPLOY, [Revision(0, "revision", touched_files)])) == 1
 
     @pytest.mark.skipif(condition="GITHUB_JOB" in os.environ,
                         reason="fatal: detected dubious ownership in repository at '/github/workspace'")
@@ -30,6 +30,6 @@ class TestDiscovery:
         repo = test_data.get_repo()
         projects = set(load_projects(repo.root_dir(), repo.find_projects()))
         invalidated = find_invalidated_projects_for_stage(projects, Stage.BUILD,
-                                                          [History(0, "revision", {'tests/projects/job/file.py',
+                                                          [Revision(0, "hash", {'tests/projects/job/file.py',
                                                                                    'tests/some_file.txt'})])
         assert 1 == len(invalidated)
