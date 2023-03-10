f824ad00446eab0dd11c66d4116f623977264da1
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-10 19:29:56 +0800

    [ARC-580] Test sub process
    branch: feature/ARC-580-create-a-tutorial-that-gets-people-started

---
+++ tests/utilities/test_subprocess.py
@@ -0,0 +1,14 @@
+import logging
+
+from src.mpyl.utilities.subprocess import custom_check_output
+
+
+class TestSubProcess:
+
+    def test_should_capture_output(self):
+        output = custom_check_output(logging.getLogger(), "ls -la")
+        assert output.success
+
+    def test_should_handle_invalid_command(self):
+        output = custom_check_output(logging.getLogger(), "invalidcommand")
+        assert not output.success
