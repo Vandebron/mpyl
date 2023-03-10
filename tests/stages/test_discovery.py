327b2b3fd0dbd2ca366bb6dec6e7d931f6681d8a
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-10 19:14:41 +0800

    [ARC-580] Test invalidation logic independent from git
    branch: feature/ARC-580-create-a-tutorial-that-gets-people-started

--- tests/stages/test_discovery.py
+++ tests/stages/test_discovery.py
@@ -4,8 +4,9 @@ import pytest
 
 from src.mpyl.project import Stage
 from src.mpyl.projects.find import load_projects
-from src.mpyl.utilities.repo import Revision
 from src.mpyl.stages.discovery import find_invalidated_projects_for_stage
+from src.mpyl.utilities.repo import Revision
+from tests import root_test_path
 from tests.test_resources import test_data
 
 
@@ -24,12 +25,11 @@ class TestDiscovery:
         assert len(
             find_invalidated_projects_for_stage(projects, Stage.DEPLOY, [Revision(0, "revision", touched_files)])) == 1
 
-    @pytest.mark.skipif(condition="GITHUB_JOB" in os.environ,
-                        reason="fatal: detected dubious ownership in repository at '/github/workspace'")
     def test_should_find_invalidated_dependencies(self):
-        repo = test_data.get_repo()
-        projects = set(load_projects(repo.root_dir(), repo.find_projects()))
+        projs = {'projects/job/deployment/project.yml', 'projects/service/deployment/project.yml',
+                 'projects/sbt-service/deployment/project.yml'}
+        projects = set(load_projects(root_test_path, projs))
         invalidated = find_invalidated_projects_for_stage(projects, Stage.BUILD,
-                                                          [Revision(0, "hash", {'tests/projects/job/file.py',
-                                                                                   'tests/some_file.txt'})])
+                                                          [Revision(0, "hash", {'projects/job/file.py',
+                                                                                'some_file.txt'})])
         assert 1 == len(invalidated)
