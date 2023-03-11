0e99ea1d491be48572bec78f26faab5a1c48f18c
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-11 11:55:41 +0700

    [ARC-595] Add cli scaffolding
    branch: feature/ARC-595-mpyl-basic-cli

--- src/mpyl/__init__.py
+++ src/mpyl/__init__.py
@@ -5,3 +5,22 @@
 
 .. include:: ../../README-dev.md
 """
+
+import click
+
+from .cli.meta_info import MetaInfo
+
+
+@click.command()
+@click.option('--version', help='Shows MPyL version', is_flag=True)
+@click.option('--about', help='Shows legal info and metadata', is_flag=True)
+def main(version, about):
+    """Command Line Interface for MPyL"""
+    if version:
+        MetaInfo().print_version()
+    if about:
+        MetaInfo().print_about()
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable = no-value-for-parameter
