04d0bedb16bc7d219fce5a0b5ae28f1b95a9b31d
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-12 14:29:35 +0700

    [ARC-595] Add linting command
    branch: feature/ARC-595-mpyl-basic-cli

--- src/mpyl/projects/find.py
+++ src/mpyl/projects/find.py
@@ -1,7 +1,8 @@
 """ Loads all projects inside a repository. """
+from pathlib import Path
 
 from ..project import Project, load_project
 
 
-def load_projects(root_dir: str, paths: set[str]) -> set[Project]:
-    return set(map(lambda p: load_project(root_dir, p, False), paths))
+def load_projects(root_dir: Path, paths: set[str]) -> set[Project]:
+    return set(map(lambda p: load_project(root_dir, Path(p), False), paths))
