ce49ec71a1a907a4a8728027a6138b69ad0d02f2
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-12 20:42:48 +0700

    [ARC-595] Add simple Jenkins build runner
    branch: feature/ARC-595-mpyl-basic-cli

---
+++ src/mpyl/cli/build/jenkins.py
@@ -0,0 +1,50 @@
+"""Jenkins multibranch pipeline build tool"""
+from dataclasses import dataclass
+
+import requests
+from github import Github
+from jenkinsapi.jenkins import Jenkins
+from rich.console import Console
+
+from ...project import Target
+from ...utilities.github import GithubConfig, get_pr_for_branch
+from ...utilities.jenkins import JenkinsConfig, Pipeline
+from ...utilities.jenkins.runner import JenkinsRunner
+from ...utilities.pyaml_env import parse_config
+from ...utilities.repo import RepoConfig
+from ...utilities.repo import Repository
+
+
+@dataclass(frozen=True)
+class JenkinsRunParameters:
+    jenkins_user: str
+    jenkins_password: str
+    config_path: str
+    pipeline: str
+
+
+def run_build(run_config: JenkinsRunParameters):
+    log_console = Console(log_path=False, log_time=False)
+    with log_console.status('Fetching Github info.. [blue]>gh pr view[/blue]') as status:
+        config = parse_config('config.yml')
+        github_config = GithubConfig(config)
+        with Repository(RepoConfig(config)) as git_repo:
+            github = Github(login_or_token=github_config.token)
+            repo = github.get_repo(github_config.repository)
+
+            pull = get_pr_for_branch(repo, git_repo.get_branch)
+
+            jenkins_config = JenkinsConfig.from_config(config)
+            pipeline_info = Pipeline(target=Target.PULL_REQUEST, tag=f'{pull.number}', url=pull.url,
+                                     pipeline=run_config.pipeline, body=pull.body, jenkins_config=jenkins_config)
+
+            status.start()
+            status.update(f'Fetching Jenkins info for {pipeline_info.human_readable()} ...')
+
+            try:
+                runner = JenkinsRunner(pipeline=pipeline_info,
+                                       jenkins=Jenkins(jenkins_config.url, username=run_config.jenkins_user,
+                                                       password=run_config.jenkins_password), status=status)
+                runner.run()
+            except requests.ConnectionError:
+                status.console.log('⚠️ Could not connect to Jenkins. Are you on VPN?')
