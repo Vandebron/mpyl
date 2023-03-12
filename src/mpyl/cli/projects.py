a4e298bae9e4bba6d0dc2afa75b87249c82d7e2a
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-12 14:29:35 +0700

    [ARC-595] Add linting command
    branch: feature/ARC-595-mpyl-basic-cli

--- src/mpyl/cli/projects.py
+++ src/mpyl/cli/projects.py
@@ -1,6 +1,10 @@
 """Commands related to projects and how they relate"""
-import click
+from pathlib import Path
 
+import click
+import jsonschema
+
+from ..project import validate_project
 from ..utilities.pyaml_env import parse_config
 from ..utilities.repo import Repository, RepoConfig
 
@@ -14,16 +18,26 @@ def projects(ctx, config):
     ctx.obj = ctx.with_resource(Repository(config=RepoConfig(parsed_config)))
 
 
-@projects.command(name='list')
+@projects.command(name='list', help='List found projects')
 @click.pass_obj
 def list_projects(repo):
     found_projects = repo.find_projects()
     click.echo(f'projects {found_projects}')
 
 
-@projects.command()
-def lint():
-    click.echo('projects list')
+@projects.command(help='Validate the yaml of found projects against their schema')
+@click.pass_obj
+def lint(repo):
+    found_projects: set[str] = repo.find_projects()
+    for project in found_projects:
+        try:
+            project_path = Path('.') / Path(project)
+            with open(project_path, encoding='utf-8') as file:
+                validate_project(file)
+        except jsonschema.exceptions.ValidationError as exc:
+            click.echo(f'❌ {project}: {exc.message}')
+        else:
+            click.echo(f'✅ {project}')
 
 
 if __name__ == '__main__':
