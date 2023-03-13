6f7a0e7beb0ed9a95b4262af19417d4107491861
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-13 17:22:46 +0700

    [ARC-584] Small fixes
    branch: feature/ARC-584-cli-small-fixes

--- src/mpyl/__init__.py
+++ src/mpyl/__init__.py
@@ -1,6 +1,38 @@
 """
 .. include:: ../../README.md
 
+## Technologies
+
+### Requirements
+The following technologies are expected to be present on the local OS:
+ - [Python](https://www.python.org/) >= 3.9
+ - [Pip](https://pypi.org/project/pip/) >= 23.0.1
+ - [Pipenv](https://pypi.org/project/pipenv/) >= 2023.2.18
+ - [Docker](https://www.docker.com/) > 20
+ - [Docker compose](https://docs.docker.com/compose/install/linux/)
+ installed as plugin (`docker compose version`) >= v2.2.3
+ - [Git](https://git-scm.com/) SCM
+
+### Bundled
+MPyL is extensible and has a minimal footprint. Having said that, batteries for the following technologies are included.
+
+##### CI/CD
+###### Build
+ - [Docker](https://www.docker.com/) `mpyl.steps.build.dockerbuild`
+ - [Scala (SBT)](https://www.scala-sbt.org/) `mpyl.steps.build.sbt`
+
+###### Testing
+ - [Junit](https://junit.org/) `mpyl.steps.models.ArtifactType.JUNIT_TESTS`
+
+###### Deployment
+ - [K8S](https://kubernetes.io/) `mpyl.steps.deploy.kubernetes`
+ - [Helm](https://helm.sh/) `mpyl.steps.deploy.k8s.helm`
+
+##### Reporting
+ - [Jira](https://www.atlassian.com) `mpyl.reporting.targets.jira`
+ - [Github](https://github.com/) `mpyl.reporting.targets.github`
+ - [Slack](https://slack.com/) `mpyl.reporting.targets.slack`
+
 .. include:: ../../README-usage.md
 
 .. include:: ../../README-dev.md
