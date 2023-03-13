22ebef4bf8b28fc9d43ef73409b7d2ebe147af65
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-13 12:37:14 +0700

    [ARC-595] Select pipeline to run from config, with default
    branch: feature/ARC-595-mpyl-basic-cli

--- src/mpyl/cli/commands/build.py
+++ src/mpyl/cli/commands/build.py
@@ -1,7 +1,10 @@
 """Commands related to build"""
+from typing import Any, Optional
+
 import click
 
 from ..build.jenkins import JenkinsRunParameters, run_build
+from ...utilities.pyaml_env import parse_config
 
 
 @click.group('build')
@@ -11,12 +14,33 @@ def build(ctx, config):
     """Pipeline build commands"""
     if ctx.obj is None:
         ctx.obj = {}
-    ctx.obj["config_path"] = config
+    ctx.obj["config"] = parse_config(config)
 
 
 @build.command()
 def status():
     click.echo('build status')
+
+
+def get_default(ctx):
+    if not ctx:
+        return 'config.jenkins.defaultPipeline'
+
+    return ctx.obj['config']['jenkins']['defaultPipeline']
+
+
+class DynamicChoice(click.Choice):
+    def __init__(self):
+        super().__init__([])
+
+    def convert(
+            self, value: Any, param: Optional["Parameter"], ctx: Optional["Context"]
+    ) -> Any:
+        parsed = ctx.obj['config']
+        if value is None:
+            value = parsed['jenkins']['defaultPipeline']
+        self.choices = parsed['jenkins']['pipelines'].keys()
+        return super().convert(value, param, ctx)
 
 
 @build.command()
@@ -34,9 +58,17 @@ def status():
     type=click.STRING,
     required=True
 )
+@click.option(
+    '--pipeline', '-pl',
+    help='The pipeline to run. Must be one of the pipelines listed in `jenkins.pipelines`. '
+         'Default value is `jenkins.defaultPipeline',
+    type=DynamicChoice(),
+    required=True,
+    default=lambda: get_default(click.get_current_context(silent=True))
+)
 @click.pass_obj
-def run(obj, user, password):
-    run_argument = JenkinsRunParameters(user, password, obj['config_path'], 'mpyl-test')
+def run(obj, user, password, pipeline):
+    run_argument = JenkinsRunParameters(user, password, obj['config'], pipeline)
     run_build(run_argument)
 
 
