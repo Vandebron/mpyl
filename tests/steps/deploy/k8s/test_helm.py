3f0666c5db9e41b718514b7f42fc51369a2f803e
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-14 13:40:28 +0700

    [ARC-524] Pass chart builder to helm deploy
    to allow for more control
    
    branch: feature/ARC-524-K8s-deploy-job-fixup

--- tests/steps/deploy/k8s/test_helm.py
+++ tests/steps/deploy/k8s/test_helm.py
@@ -1,6 +1,7 @@
 import tempfile
 
 from src.mpyl.steps import Input
+from src.mpyl.steps.deploy.k8s.chart import ChartBuilder, to_service_chart
 from src.mpyl.steps.deploy.k8s.helm import write_chart, to_chart_metadata
 from tests.test_resources import test_data
 from tests.test_resources.test_data import get_project
@@ -12,4 +13,5 @@ class TestHelm:
         step_input = Input(get_project(), test_data.RUN_PROPERTIES, required_artifact=output.produced_artifact,
                            dry_run=True)
         with tempfile.TemporaryDirectory() as tempdir:
-            write_chart(step_input, tempdir, to_chart_metadata('chart_name', test_data.RUN_PROPERTIES))
+            builder = ChartBuilder(step_input)
+            write_chart(to_service_chart(builder), tempdir, to_chart_metadata('chart_name', test_data.RUN_PROPERTIES))
