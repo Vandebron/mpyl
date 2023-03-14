4e716e33a0918b589130cb1ed4fbfa2ace783b26
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-14 13:40:28 +0700

    [ARC-524] Pass chart builder to helm deploy
    to allow for more control
    
    branch: feature/ARC-524-K8s-deploy-job-fixup

--- src/mpyl/steps/deploy/k8s/helm.py
+++ src/mpyl/steps/deploy/k8s/helm.py
@@ -6,9 +6,8 @@ import shutil
 from logging import Logger
 from pathlib import Path
 
-from .chart import ChartBuilder, to_service_chart
-from .resources.crd import to_yaml
-from ...models import RunProperties, Input, Output
+from .resources.crd import to_yaml, CustomResourceDefinition
+from ...models import RunProperties, Output, Input
 from ....utilities.subprocess import custom_check_output
 
 
@@ -22,9 +21,7 @@ appVersion: "{run_properties.versioning.identifier}"
 """
 
 
-def write_chart(step_input: Input, chart_path: Path, chart_metadata: str) -> None:
-    builder = ChartBuilder(step_input)
-
+def write_chart(chart: dict[str, CustomResourceDefinition], chart_path: Path, chart_metadata: str) -> None:
     shutil.rmtree(chart_path, ignore_errors=True)
     template_path = chart_path / Path("templates")
     template_path.mkdir(parents=True, exist_ok=True)
@@ -34,18 +31,17 @@ def write_chart(step_input: Input, chart_path: Path, chart_metadata: str) -> Non
     with open(chart_path / Path("values.yaml"), mode='w+', encoding='utf-8') as file:
         file.write("# This file is intentionally left empty. All values in /templates have been pre-interpolated")
 
-    templates = to_service_chart(builder)
-    my_dictionary: dict[str, str] = dict(map(lambda item: (item[0], to_yaml(item[1])), templates.items()))
+    my_dictionary: dict[str, str] = dict(map(lambda item: (item[0], to_yaml(item[1])), chart.items()))
 
     for name, template in my_dictionary.items():
         with open(template_path / name, mode='w+', encoding='utf-8') as file:
             file.write(template)
 
 
-def install(logger: Logger, step_input: Input, name_space: str, kube_context: str) -> Output:
-    chart_name = step_input.project.name.lower()
+def install(logger: Logger, chart: dict[str, CustomResourceDefinition], step_input: Input, chart_name: str,
+            name_space: str, kube_context: str) -> Output:
     chart_path = Path(step_input.project.target_path) / "chart"
-    write_chart(step_input, chart_path, to_chart_metadata(chart_name, step_input.run_properties))
+    write_chart(chart, chart_path, to_chart_metadata(chart_name, step_input.run_properties))
 
     cmd = f"helm upgrade -i {chart_name} -n {name_space} --kube-context {kube_context} {chart_path}"
     if step_input.dry_run:
