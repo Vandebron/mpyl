23cad7e2c4c2d6f39b3cf9e0bad591cdd50afe9d
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-14 14:49:13 +0700

    [ARC-524] Do actual deployment in kubernetes job deploy
    branch: feature/ARC-524-K8s-deploy-job-fixup

--- src/mpyl/steps/deploy/kubernetes_job.py
+++ src/mpyl/steps/deploy/kubernetes_job.py
@@ -2,6 +2,8 @@
 
 from logging import Logger
 
+from .k8s import deploy_helm_chart
+from .k8s.chart import ChartBuilder, to_job_chart
 from .. import Step, Meta
 from ..models import Input, Output, ArtifactType
 from ...project import Stage
@@ -18,4 +20,5 @@ class DeployKubernetesJob(Step):
         ), produced_artifact=ArtifactType.NONE, required_artifact=ArtifactType.DOCKER_IMAGE)
 
     def execute(self, step_input: Input) -> Output:
-        return Output(success=True, message='Dummy Implementation')
+        builder = ChartBuilder(step_input)
+        return deploy_helm_chart(self._logger, to_job_chart(builder), step_input, builder.release_name)
