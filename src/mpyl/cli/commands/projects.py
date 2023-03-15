014df8825a771eaf1a61dc1b5e40628a51791ebf
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-15 14:35:40 +0700

    [ARC-595] Enable logger for use in cli
    branch: feature/ARC-595-cli-improvements

--- src/mpyl/cli/commands/projects.py
+++ src/mpyl/cli/commands/projects.py
@@ -1,45 +1,58 @@
 """Commands related to projects and how they relate"""
+import logging
+from dataclasses import dataclass
+from logging import Logger
 from pathlib import Path
 
 import click
 import jsonschema
 
+from .. import set_cli_logger
 from ...project import validate_project
 from ...utilities.pyaml_env import parse_config
 from ...utilities.repo import Repository, RepoConfig
 
 
+@dataclass(frozen=True)
+class ProjectsContext:
+    config: dict
+    repo: Repository
+    logger: Logger
+
+
 @click.group('projects')
 @click.option('--config', '-c', required=True, type=click.Path(exists=True), help='Path to config.yml',
-              default='config.yml')
+              envvar="MPYL_CONFIG_PATH", default='config.yml')
 @click.pass_context
 def projects(ctx, config):
     """Commands related to projects"""
     parsed_config = parse_config(config)
-    ctx.obj = ctx.with_resource(Repository(config=RepoConfig(parsed_config)))
+    set_cli_logger(local=False, verbose=False)
+    ctx.obj = ProjectsContext(parsed_config, ctx.with_resource(Repository(config=RepoConfig(parsed_config))),
+                              logger=logging.getLogger())
 
 
 @projects.command(name='list', help='List found projects')
 @click.pass_obj
-def list_projects(repo):
-    found_projects = repo.find_projects()
-    for proj in found_projects:
-        click.echo(proj)
+def list_projects(obj: ProjectsContext):
+    found_projects = obj.repo.find_projects()
+    for proj in sorted(found_projects):
+        obj.logger.info(proj)
 
 
 @projects.command(help='Validate the yaml of found projects against their schema')
 @click.pass_obj
-def lint(repo):
-    found_projects: set[str] = repo.find_projects()
-    for project in found_projects:
+def lint(obj: ProjectsContext):
+    found_projects: set[str] = obj.repo.find_projects()
+    for project in sorted(found_projects):
         try:
             project_path = Path('.') / Path(project)
             with open(project_path, encoding='utf-8') as file:
                 validate_project(file)
         except jsonschema.exceptions.ValidationError as exc:
-            click.echo(f'❌ {project}: {exc.message}')
+            obj.logger.info(f'❌ {project}: {exc.message}')
         else:
-            click.echo(f'✅ {project}')
+            obj.logger.info(f'✅ {project}')
 
 
 if __name__ == '__main__':
