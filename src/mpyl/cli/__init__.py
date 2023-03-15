080df13a9704d59be0b57acf9fdb6146c9767d70
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-15 14:35:40 +0700

    [ARC-595] Enable logger for use in cli
    branch: feature/ARC-595-cli-improvements

--- src/mpyl/cli/__init__.py
+++ src/mpyl/cli/__init__.py
@@ -1,1 +1,24 @@
 """Command Line Interface for MPyL"""
+import logging
+
+from rich.console import Console
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+
+
+def create_console_logger(local: bool, verbose: bool) -> Console:
+    console = Console(markup=True, width=None if local else 135, no_color=False, log_path=False, color_system='256')
+    logging.basicConfig(
+        level="DEBUG" if verbose else "INFO", format=FORMAT, datefmt="[%X]",
+        handlers=[RichHandler(markup=True,
+                              console=console, show_path=local)]
+    )
+    return console
+
+
+def set_cli_logger(local: bool, verbose: bool):
+    logging.basicConfig(
+        level="DEBUG" if verbose else "INFO", format=FORMAT, datefmt="[%X]",
+        handlers=[RichHandler(markup=True, show_path=local, show_level=False, show_time=False)]
+    )
