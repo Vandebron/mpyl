d2338e940a8f7174d513c2725e15dcd4e4980520
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-20 15:11:34 +0700

    [ARC-584] Display newer version if it is available
    branch: feature/ARC-584-discover-service-dependencies

--- src/mpyl/cli/__init__.py
+++ src/mpyl/cli/__init__.py
@@ -4,6 +4,7 @@ import logging
 from dataclasses import dataclass
 from importlib.metadata import version as version_meta
 
+import requests
 from rich.console import Console
 from rich.logging import RichHandler
 
@@ -24,7 +25,15 @@ class CliContext:
 
 def get_version():
     try:
-        return f"v{version_meta('mpyl')}"
+        meta = version_meta('mpyl')
+        try:
+            resp = requests.get("https://pypi.org/pypi/mpyl/json", timeout=3)
+            latest = resp.json().get('info', {}).get('version')
+            if meta != latest:
+                return f"v{meta}. ⚠️ \033[1;33;40m A newer version is available: `pip install -U {latest}`"
+        except requests.exceptions.RequestException:
+            pass
+        return f"v{meta}"
     except importlib.metadata.PackageNotFoundError:
         return '(local)'
 
