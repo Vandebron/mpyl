2f4af8528f99a091f95c298493274bb27c465d7f
ewaldschrader <ewaldschrader@vandebron.nl>
2023-03-22 16:37:20 +0100

    Improve validation test for run_properties
    branch:

--- tests/steps/test_models.py
+++ tests/steps/test_models.py
@@ -13,23 +13,18 @@ yaml.preserve_quotes = True
 class TestModels:
     resource_path = root_test_path / "test_resources"
 
-    def test_should_return_error_if_validation_fails(self):
-        run_properties_values = parse_config(self.resource_path / "run_properties.yml")
-        config_values = parse_config(self.resource_path / "config.yml")
+    run_properties_values = parse_config(resource_path / "run_properties.yml")
+    config_values = parse_config(resource_path / "config.yml")
 
-        del run_properties_values['build']['run']['user']
+    def test_should_return_error_if_validation_fails(self):
 
         with pytest.raises(ValidationError) as excinfo:
-            RunProperties.from_configuration(run_properties_values, config_values)
+            RunProperties.from_configuration(self.run_properties_values, self.config_values)
 
         assert "'user' is a required property" in excinfo.value.message
 
     def test_should_return_error_if_pr_number_or_tag_not_set(self):
-        run_properties_values = parse_config(self.resource_path / "run_properties.yml")
-        versioning = run_properties_values['build']['versioning']
-
-        versioning['pr_number'] = None
-        versioning['tag'] = None
+        versioning = self.run_properties_values['build']['versioning']
 
         with pytest.raises(ValueError, match="Either pr_number or tag need to be set"):
             VersioningProperties(versioning['revision'], versioning['branch'], versioning['pr_number'], versioning['tag'])
