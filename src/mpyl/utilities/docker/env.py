a484b31693ab73cd3b110a76cc8d69497dc9ae6c
Jorg88 <jorgpost@vandebron.nl>
2023-03-27 11:18:18 +0200

    [ARC-604] Fix the ephemeral test step
    branch: feature/ARC-604-implement-ephemeral-deploy-step

--- src/mpyl/utilities/docker/env.py
+++ src/mpyl/utilities/docker/env.py
@@ -1,14 +1,13 @@
-from mpyl import parse_config
-from mpyl.project import Project, Target
+from mpyl.project import Project
 from mpyl.steps.models import RunProperties
 
 
 def write_env_to_file(project: Project, run_properties: RunProperties) -> str:
     env_str: str = '\n'.join([f'{e.key}={e.get_value(run_properties.target)}' for e in project.deployment.properties.env])
-
     env_file_name: str = run_properties.config['docker']['envFileName']
-    env_file = open(env_file_name, 'x')
-    env_file.write(env_str)
-    env_file.close()
+
+    with open(env_file_name, 'w+') as env_file:
+        env_file.write(env_str)
+        env_file.close()
 
     return env_file_name
