92c5bbd9880d24fed60c1c01e9456038d8d19567
Jorg88 <jorgpost@vandebron.nl>
2023-03-28 12:48:12 +0200

    [ARC-604] Use docker.run instead of custom cmd with env file
    branch: feature/ARC-604-implement-ephemeral-deploy-step

--- tests/steps/deploy/ephemeral/test_ephemeral.py
+++ tests/steps/deploy/ephemeral/test_ephemeral.py
@@ -1,9 +1,8 @@
-import os
 from pathlib import Path
 
 from mpyl.project import load_project
 from mpyl.steps.models import Input
-from mpyl.utilities.docker import write_env_to_file
+from mpyl.utilities.docker import get_env_variables
 from tests import root_test_path
 from tests.test_resources import test_data
 
@@ -11,14 +10,9 @@ from tests.test_resources import test_data
 class TestEphemeral:
     resource_path = root_test_path / 'projects' / 'ephemeral' / 'deployment'
 
-    def test_replace_default_with_none(self):
+    def test_get_env_variables_for_target(self):
         step_input = Input(load_project(self.resource_path, Path('project.yml'), True), test_data.RUN_PROPERTIES, None)
         assert len(step_input.project.deployment.properties.env) == 4
-        write_env_to_file(step_input.project, test_data.RUN_PROPERTIES)
-        env_file_name = step_input.run_properties.config['docker']['envFileName']
 
-        with open(env_file_name, 'r') as file:
-            envs = file.readlines()
-            assert len(envs) == 4
-
-        os.remove(env_file_name)
+        env_variables = get_env_variables(step_input.project, test_data.RUN_PROPERTIES)
+        assert len(env_variables) == 4
