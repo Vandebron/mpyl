bb598e4a0a4d82368f7e039cec03ca7856a0f40a
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-03-29 20:45:03 +0800

    [ARC-535] Create mock test results in Echo step
    to make it more realistic
    
    branch: feature/ARC-535-target-specific-dictionary

--- src/mpyl/steps/test/echo.py
+++ src/mpyl/steps/test/echo.py
@@ -3,8 +3,9 @@
 from logging import Logger
 
 from .. import Step, Meta
-from ..models import Input, Output, ArtifactType
+from ..models import Input, Output, ArtifactType, input_to_artifact
 from ...project import Stage
+from ...utilities.junit import TEST_OUTPUT_PATH_KEY
 
 
 class TestEcho(Step):
@@ -15,8 +16,10 @@ class TestEcho(Step):
             description='Dummy test step to test the framework',
             version='0.0.1',
             stage=Stage.TEST
-        ), ArtifactType.NONE, ArtifactType.NONE)
+        ), produced_artifact=ArtifactType.JUNIT_TESTS, required_artifact=ArtifactType.NONE)
 
     def execute(self, step_input: Input) -> Output:
         self._logger.info(f"Testing project {step_input.project.name}")
-        return Output(success=True, message=f"Tested {step_input.project.name}", produced_artifact=None)
+        artifact = input_to_artifact(artifact_type=ArtifactType.JUNIT_TESTS, step_input=step_input,
+                                     spec={TEST_OUTPUT_PATH_KEY: 'target/tests'})
+        return Output(success=True, message=f"Tested {step_input.project.name}", produced_artifact=artifact)
