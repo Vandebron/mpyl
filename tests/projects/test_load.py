d519c7bdf443b4ac176d51ab2f8502ce62851c5f
Jorg88 <jorgpost@vandebron.nl>
2023-03-30 16:48:40 +0200

    [ARC-606] Fix the schema validation tests since 'with repo' was not properly validating
    branch: feature/ARC-606-implement-spark-deploy-step

--- tests/projects/test_load.py
+++ tests/projects/test_load.py
@@ -6,14 +6,14 @@ from tests.test_resources import test_data
 class TestProjectLoad:
 
     def test_load_all_projects(self):
-        with test_data.get_repo() as repo:
-            projects = load_projects(repo.root_dir(), repo.find_projects())
+        repo = test_data.get_repo()
+        projects = load_projects(repo.root_dir(), repo.find_projects(), True)
         assert len(projects) == 4
 
     def test_load_all_project_dependencies(self):
-        with test_data.get_repo() as repo:
-            dependencies = find_all_dependencies(repo.root_dir(), repo.find_projects())
-            deps: dict[str, ProjectWithDependents] = dict(map(lambda d: (d.name, d), dependencies))
+        repo = test_data.get_repo()
+        dependencies = find_all_dependencies(repo.root_dir(), repo.find_projects())
+        deps: dict[str, ProjectWithDependents] = dict(map(lambda d: (d.name, d), dependencies))
 
         assert len(dependencies) == 4
         assert len(deps['job'].dependent_projects) == 1
