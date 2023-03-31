784a70209430a2a47994b3a6613836a4f5dee1ca
koeves <daniel.koves@icloud.com>
2023-03-31 18:02:44 +0200

    [ARC-606] Fix schema and test crd validation
    branch: feature/ARC-606-implement-spark-deploy-step

--- tests/steps/deploy/k8s/test_crd.py
+++ tests/steps/deploy/k8s/test_crd.py
@@ -1,17 +1,24 @@
 from pathlib import Path
 
+import pytest
+
 from mpyl.project import load_project
 from mpyl.steps import Input
 from mpyl.steps.deploy.k8s.chart import ChartBuilder
+from mpyl.steps.deploy.k8s.resources.crd import to_yaml
 from test_resources import test_data
 from tests import root_test_path
 
 
+def get_builder_for_resource(resource_path: Path) -> ChartBuilder:
+    step_input = Input(load_project(resource_path, Path('project.yml'), True), test_data.RUN_PROPERTIES, None)
+    return ChartBuilder(step_input)
+
+
 class TestCrdChart:
-    def test_spark_chart(self):
+    def test_construct_crd_spark_chart(self):
         resource_path = root_test_path / 'projects' / 'spark-job' / 'deployment'
-        step_input = Input(load_project(resource_path, Path('project.yml'), True), test_data.RUN_PROPERTIES, None)
-        chart = ChartBuilder(step_input).create_chart()
+        chart = get_builder_for_resource(resource_path).create_chart()
 
         assert chart['spark'] is not None
         assert chart['spark'].spec['schedule'] == '0 7 * * *'
@@ -20,3 +27,11 @@ class TestCrdChart:
                'nl.vandebron.batterypack.enrichchargesessions.EnrichChargeSessionsJobMain'
         assert chart['spark'].spec['template']['mainApplicationFile'] == \
                'local:///app/enrichchargesessionsjob-assembly-1.0.jar'
+
+    def test_validate_crd_spark_chart(self):
+        resource_path = root_test_path / 'projects' / 'spark-job' / 'deployment'
+        chart = get_builder_for_resource(resource_path).create_chart()
+        chart['spark'].spec['arguments'] = [1]  # arguments should be a list of string
+
+        with pytest.raises(ValueError):
+            to_yaml(chart['spark'])
