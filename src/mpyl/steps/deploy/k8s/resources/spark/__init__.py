03457664fd8597973cdaeecc03d9c664a4560dea
Jorg88 <jorgpost@vandebron.nl>
2023-04-04 10:29:01 +0200

    [ARC-606] Make parts of the config map body dynamic instead of static
    branch: feature/ARC-606-implement-spark-deploy-step

--- src/mpyl/steps/deploy/k8s/resources/spark/__init__.py
+++ src/mpyl/steps/deploy/k8s/resources/spark/__init__.py
@@ -1,7 +1,10 @@
 """ Utilities to construct spark application yaml """
 
+from ......project import Project, Target
+from ......utilities.ephemeral import get_env_variables
 
-def to_spark_body(spark: dict[str, str]) -> dict:
+
+def to_spark_body(project: Project, target: Target) -> dict:
     static_body = {
         'type': 'Scala',
         'mode': 'cluster',
@@ -10,7 +13,7 @@ def to_spark_body(spark: dict[str, str]) -> dict:
         'restartPolicy': {
             'type': 'Never'
         },
-        'sparkConfigMap': 'enrichChargeSessionsJob',
+        'sparkConfigMap': project.name,
         'image': 'bigdataregistry.azurecr.io/send-slack-notification:PR-1231',
         'arguments': [
             'python',
@@ -25,11 +28,8 @@ def to_spark_body(spark: dict[str, str]) -> dict:
             'labels': {
                 'version': '3.1.1'
             },
-            'serviceAccount': 'release-name-job',
-            'envVars': {
-                'DA_SLACK_BOT_TOKEN': 'xoxb-65778094515-2212272125044-E7qttf3XLQi2vaL0dGkBstLD',
-                'DEPLOY_ENV': 'test'
-            },
+            'serviceAccount': project.name,
+            'envVars': get_env_variables(project, target),
             'envSecretKeyRefs': None
         },
         'executor': {
@@ -40,10 +40,7 @@ def to_spark_body(spark: dict[str, str]) -> dict:
             'labels': {
                 'version': '3.1.1'
             },
-            'envVars': {
-                'DA_SLACK_BOT_TOKEN': 'xoxb-65778094515-2212272125044-E7qttf3XLQi2vaL0dGkBstLD',
-                'DEPLOY_ENV': 'test'
-            },
+            'envVars': get_env_variables(project, target),
             'envSecretKeyRefs': None
         },
         'deps': {
@@ -60,56 +57,57 @@ def to_spark_body(spark: dict[str, str]) -> dict:
         }
     }
 
-    return static_body | spark
+    return static_body | project.kubernetes.spark
 
 
 def get_spark_config_map_data() -> dict:
     return {
-        'log4j.properties': '#\n'
-                            '# Licensed to the Apache Software Foundation (ASF) under one or more\n'
-                            '# contributor license agreements.  See the NOTICE file distributed with\n'
-                            '# this work for additional information regarding copyright ownership.\n'
-                            '# The ASF licenses this file to You under the Apache License, Version 2.0\n'
-                            '# (the "License"); you may not use this file except in compliance with\n'
-                            '# the License.  You may obtain a copy of the License at\n'
-                            '#\n'
-                            '#    http://www.apache.org/licenses/LICENSE-2.0\n'
-                            '#\n'
-                            '# Unless required by applicable law or agreed to in writing, software\n'
-                            '# distributed under the License is distributed on an "AS IS" BASIS,\n'
-                            '# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n'
-                            '# See the License for the specific language governing permissions and\n'
-                            '# limitations under the License.\n'
-                            '#\n'
-                            '#log everything to file at the beginning\n'
-                            'log4j.rootCategory=INFO, console, sparklog\n'
-                            '#afterwards log only our messages to the console\n'
-                            'log4j.logger.nl.vandebron.sparkplug=INFO, console\n'
-                            '#configure ConsoleAppender\n'
-                            'log4j.appender.console=org.apache.log4j.ConsoleAppender\n'
-                            'log4j.appender.console.target=System.out\n'
-                            'log4j.appender.console.layout=org.apache.log4j.PatternLayout\n'
-                            'log4j.appender.console.layout.ConversionPattern=%d{yy/MM/dd HH:mm:ss} %p %c{1}: %m%n\n'
-                            'log4j.appender.sparklog=org.apache.log4j.FileAppender\n'
-                            '#this must be overwritten programmatically when setting the appender\n'
-                            'log4j.appender.sparklog.File=/dev/null\n'
-                            'log4j.appender.sparklog.layout=org.apache.log4j.PatternLayout\n'
-                            'log4j.appender.sparklog.layout.ConversionPattern=%d %-5p [%c{1}] %m%n\n'
-                            'log4j.appender.sparklog.Threshold=INFO\n'
-                            'log4j.appender.sparklog.Append=true\n'
-                            '# Set the default spark-shell log level to WARN. When running the spark-shell, the\n'
-                            '# log level for this class is used to overwrite the root logger\'s log level, so that\n'
-                            '# the user can have different defaults for the shell and regular Spark apps.\n'
-                            'log4j.logger.org.apache.spark.repl.Main=WARN\n'
-                            '# Settings to quiet third party logs that are too verbose\n'
-                            'log4j.logger.org.spark_project.jetty=WARN\n'
-                            'log4j.logger.org.spark_project.jetty.util.component.AbstractLifeCycle=ERROR\n'
-                            'log4j.logger.org.apache.spark.repl.SparkIMain$exprTyper=INFO\n'
-                            'log4j.logger.org.apache.spark.repl.SparkILoop$SparkILoopInterpreter=INFO\n'
-                            'log4j.logger.org.apache.parquet=ERROR\n'
-                            'log4j.logger.parquet=ERROR\n'
-                            # pylint: disable-next=line-too-long
-                            '# SPARK-9183: Settings to avoid annoying messages when looking up nonexistent UDFs in SparkSQL with Hive support\n'
-                            'log4j.logger.org.apache.hadoop.hive.metastore.RetryingHMSHandler=FATAL\n'
-                            'log4j.logger.org.apache.hadoop.hive.ql.exec.FunctionRegistry=ERROR',
+        'log4j.properties':
+            '#\n'
+            '# Licensed to the Apache Software Foundation (ASF) under one or more\n'
+            '# contributor license agreements.  See the NOTICE file distributed with\n'
+            '# this work for additional information regarding copyright ownership.\n'
+            '# The ASF licenses this file to You under the Apache License, Version 2.0\n'
+            '# (the "License"); you may not use this file except in compliance with\n'
+            '# the License.  You may obtain a copy of the License at\n'
+            '#\n'
+            '#    http://www.apache.org/licenses/LICENSE-2.0\n'
+            '#\n'
+            '# Unless required by applicable law or agreed to in writing, software\n'
+            '# distributed under the License is distributed on an "AS IS" BASIS,\n'
+            '# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n'
+            '# See the License for the specific language governing permissions and\n'
+            '# limitations under the License.\n'
+            '#\n'
+            '#log everything to file at the beginning\n'
+            'log4j.rootCategory=INFO, console, sparklog\n'
+            '#afterwards log only our messages to the console\n'
+            'log4j.logger.nl.vandebron.sparkplug=INFO, console\n'
+            '#configure ConsoleAppender\n'
+            'log4j.appender.console=org.apache.log4j.ConsoleAppender\n'
+            'log4j.appender.console.target=System.out\n'
+            'log4j.appender.console.layout=org.apache.log4j.PatternLayout\n'
+            'log4j.appender.console.layout.ConversionPattern=%d{yy/MM/dd HH:mm:ss} %p %c{1}: %m%n\n'
+            'log4j.appender.sparklog=org.apache.log4j.FileAppender\n'
+            '#this must be overwritten programmatically when setting the appender\n'
+            'log4j.appender.sparklog.File=/dev/null\n'
+            'log4j.appender.sparklog.layout=org.apache.log4j.PatternLayout\n'
+            'log4j.appender.sparklog.layout.ConversionPattern=%d %-5p [%c{1}] %m%n\n'
+            'log4j.appender.sparklog.Threshold=INFO\n'
+            'log4j.appender.sparklog.Append=true\n'
+            '# Set the default spark-shell log level to WARN. When running the spark-shell, the\n'
+            '# log level for this class is used to overwrite the root logger\'s log level, so that\n'
+            '# the user can have different defaults for the shell and regular Spark apps.\n'
+            'log4j.logger.org.apache.spark.repl.Main=WARN\n'
+            '# Settings to quiet third party logs that are too verbose\n'
+            'log4j.logger.org.spark_project.jetty=WARN\n'
+            'log4j.logger.org.spark_project.jetty.util.component.AbstractLifeCycle=ERROR\n'
+            'log4j.logger.org.apache.spark.repl.SparkIMain$exprTyper=INFO\n'
+            'log4j.logger.org.apache.spark.repl.SparkILoop$SparkILoopInterpreter=INFO\n'
+            'log4j.logger.org.apache.parquet=ERROR\n'
+            'log4j.logger.parquet=ERROR\n'
+            # pylint: disable-next=line-too-long
+            '# SPARK-9183: Settings to avoid annoying messages when looking up nonexistent UDFs in SparkSQL with Hive support\n'
+            'log4j.logger.org.apache.hadoop.hive.metastore.RetryingHMSHandler=FATAL\n'
+            'log4j.logger.org.apache.hadoop.hive.ql.exec.FunctionRegistry=ERROR',
     }
