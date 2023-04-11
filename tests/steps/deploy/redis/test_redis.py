40ffae0d452fb5c3353d50d2eff04419245fe766
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-04-11 15:04:16 +0200

    [TECH-283] Compose simplistic values.yaml for redis
    branch: fetaure/TECH-283-as-a-developer-i-want-to-deploy-redis-for-my-service

---
+++ tests/steps/deploy/redis/test_redis.py
@@ -0,0 +1,33 @@
+from io import StringIO
+from logging import Logger
+
+from ruamel.yaml import YAML
+
+from src.mpyl.steps import Input
+from src.mpyl.steps.deploy.redis_cluster import RedisClusterDeploy, compose_values
+from tests import test_resource_path, root_test_path
+from tests.test_resources.test_data import get_project, RUN_PROPERTIES, assert_roundtrip
+
+
+class TestRedisStep:
+
+    def test_redis_value_yaml(self):
+        """Test the redis step."""
+
+        step = RedisClusterDeploy(Logger('test'))
+        step_input = Input(get_project(), RUN_PROPERTIES, required_artifact=None, dry_run=True)
+
+        output = step.execute(step_input)
+
+        assert output.success
+        assert output.message == "Redis cluster deploy successful for project dockertest"
+
+    def test_compose_values(self):
+        step_input = Input(get_project(), RUN_PROPERTIES, required_artifact=None, dry_run=True)
+        values_path = root_test_path / "steps" / "deploy" / "redis" / "resources"
+
+        yaml_values = compose_values(step_input)
+        stream = StringIO()
+        YAML().dump(yaml_values, stream)
+
+        assert_roundtrip(values_path / "values.yaml", stream.getvalue())
