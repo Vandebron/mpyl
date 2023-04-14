3beb1c1ea483495bc139864b751a50f207b11df5
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-04-14 12:53:32 +0200

    [TECH-283] Group service and its persistence in the same folder
    and namespace
    
    branch: fetaure/TECH-283-as-a-developer-i-want-to-deploy-redis-for-my-service

--- tests/stages/test_discovery.py
+++ tests/stages/test_discovery.py
@@ -14,7 +14,7 @@ class TestDiscovery:
 
     def test_should_find_invalidated_test_dependencies(self):
         with test_data.get_repo() as repo:
-            touched_files = {'tests/projects/service/file.py', 'tests/some_file.txt'}
+            touched_files = {'tests/projects/caching-service/service/file.py', 'tests/some_file.txt'}
             projects = set(load_projects(repo.root_dir(), repo.find_projects()))
             assert len(find_invalidated_projects_for_stage(projects, Stage.BUILD,
                                                            [Revision(0, "revision", touched_files)])) == 1
@@ -24,7 +24,7 @@ class TestDiscovery:
                                                            [Revision(0, "revision", touched_files)])) == 1
 
     def test_should_find_invalidated_dependencies(self):
-        projs = {'projects/job/deployment/project.yml', 'projects/service/deployment/project.yml',
+        projs = {'projects/job/deployment/project.yml', 'projects/caching-service/service/deployment/project.yml',
                  'projects/sbt-service/deployment/project.yml'}
         projects = set(load_projects(root_test_path, projs))
         invalidated = find_invalidated_projects_for_stage(projects, Stage.BUILD,
