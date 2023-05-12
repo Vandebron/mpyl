7a06f007f83fd3d794dd2f32761f7e01aeab76d3
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-05-12 10:15:50 +0800

    [TECH-272] Log ingress route if present
    branch: feature/TECH-272-upgrade-monorepo

--- src/mpyl/steps/deploy/kubernetes.py
+++ src/mpyl/steps/deploy/kubernetes.py
@@ -1,8 +1,9 @@
 """ Step that deploys the docker image produced in the build stage to Kubernetes, using HELM. """
-
+import re
 from logging import Logger
+from typing import Optional
 
-from .k8s import deploy_helm_chart
+from .k8s import deploy_helm_chart, CustomResourceDefinition
 from .k8s.chart import ChartBuilder, to_service_chart
 from .. import Step, Meta
 from ..models import Input, Output, ArtifactType
@@ -19,6 +20,21 @@ class DeployKubernetes(Step):
             stage=Stage.DEPLOY
         ), produced_artifact=ArtifactType.NONE, required_artifact=ArtifactType.DOCKER_IMAGE)
 
+    @staticmethod
+    def try_extract_endpoint(chart: dict[str, CustomResourceDefinition]) -> Optional[str]:
+        ingress = chart.get('ingress-https-route')
+        if ingress:
+            routes = ingress.spec.get('routes', {})
+            if routes:
+                url = routes[0].get('match')
+                return 'https://' + next(iter(re.findall(r'`(.*)`', url)))
+        return None
+
     def execute(self, step_input: Input) -> Output:
         builder = ChartBuilder(step_input)
-        return deploy_helm_chart(self._logger, to_service_chart(builder), step_input, builder.release_name)
+        chart: dict[str, CustomResourceDefinition] = to_service_chart(builder)
+        endpoint = self.try_extract_endpoint(chart)
+        if endpoint:
+            self._logger.info(f"Service {step_input.project.name} reachable at: {endpoint}")
+
+        return deploy_helm_chart(self._logger, chart, step_input, builder.release_name)
