fae82c3032e9dbe3ac770c58153e32f0adbddf1a
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-05-15 15:21:51 +0800

    [TECH-272] Simplify result markdown logic
    branch: feature/TECH-272-upgrade-monorepo

--- src/mpyl/reporting/formatting/markdown.py
+++ src/mpyl/reporting/formatting/markdown.py
@@ -18,7 +18,7 @@ def summary_to_markdown(summary: TestRunSummary):
            f"💔 {summary.errors} 🙈 {summary.skipped}"
 
 
-def __add_link(name: str, output: Output) -> str:
+def __add_link_if_service(name: str, output: Output) -> str:
     if output.produced_artifact and output.produced_artifact.artifact_type == ArtifactType.DEPLOYED_HELM_APP:
         url = output.produced_artifact.spec.get(DEPLOYED_SERVICE_KEY)
         if url:
@@ -27,17 +27,23 @@ def __add_link(name: str, output: Output) -> str:
     return name
 
 
+def wrap_project_name(proj: Project, result: list[StepResult]):
+    project_name = proj.name
+    encapsulation = '_'
+    found_result = next((r for r in result if r.project.name == project_name), None)
+    if found_result:
+        project_name = __add_link_if_service(project_name, found_result.output)
+        encapsulation = '*' if found_result.output.success else '~~'
+
+    return f'{encapsulation}{project_name}{encapsulation}'
+
+
 def __to_oneliner(result: list[StepResult], plan: set[Project]) -> str:
     project_names: list[str] = []
     if plan:
         sorted_plan = sorted(plan, key=operator.attrgetter('name'))
         for proj in sorted_plan:
-            found_result = next((r for r in result if r.project.name == proj.name), None)
-            if found_result:
-                project_names.append(f'*{__add_link(proj.name, found_result.output)}*' if found_result.output.success
-                                     else f'~~{proj.name}~~')
-            else:
-                project_names.append(f'_{proj.name}_')
+            project_names.append(wrap_project_name(proj, result))
     else:
         project_names = list(map(lambda r: f'_{r.project.name}_', result))
 
@@ -57,13 +63,14 @@ def stage_to_icon(stage: Stage):
 def markdown_for_stage(run_result: RunResult, stage: Stage):
     step_results: list[StepResult] = run_result.results_for_stage(stage)
     plan: set[Project] = run_result.plan_for_stage(stage)
-    result = ""
-    if step_results or plan:
-        result += f"{stage_to_icon(stage)}  {__to_oneliner(step_results, plan)}  \n"
-        test_results = collect_test_results(step_results)
-        if test_results:
-            result += to_markdown_test_report(
-                test_results) + f' [link]({run_result.run_properties.details.tests_url}) \n'
+    if not step_results and not plan:
+        return ''
+
+    result = f"{stage_to_icon(stage)}  {__to_oneliner(step_results, plan)}  \n"
+    test_results = collect_test_results(step_results)
+    if test_results:
+        result += to_markdown_test_report(
+            test_results) + f' [link]({run_result.run_properties.details.tests_url}) \n'
 
     return result
 
