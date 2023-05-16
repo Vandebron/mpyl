fa7c3c44f31cc14aa2616e63f82ed863df618b4d
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-05-16 10:41:01 +0800

    [TECH-402] Log stderror in case of failure
    branch: feature/TECH-402-kubernetes-job-deploy-step-should-remove-previous-helm-installation-of-the-job

--- src/mpyl/utilities/subprocess/__init__.py
+++ src/mpyl/utilities/subprocess/__init__.py
@@ -24,7 +24,7 @@ def custom_check_output(logger: Logger, command: Union[str, list[str]], capture_
             out = subprocess.check_output(command, stderr=subprocess.STDOUT).decode("utf-8")
             return Output(success=True, message=out)
 
-        with subprocess.Popen(command, stdout=subprocess.PIPE, text=True) as process:
+        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
             if not process.stdout:
                 raise RuntimeError(f'Process {command_argument} does not have an stdout')
 
@@ -33,9 +33,11 @@ def custom_check_output(logger: Logger, command: Union[str, list[str]], capture_
                     print(line.rstrip())
                 if process.poll() is not None:
                     break
-            exit_code = process.wait()
+            success = process.wait() == 0
+            if not success:
+                logger.warning(f"Subprocess failed: {process.stderr.read() if process.stderr else 'No stderr output'}")
 
-            return Output(success=exit_code == 0, message='Subprocess executed successfully')
+            return Output(success=success, message='Subprocess executed successfully')
 
 
     except subprocess.CalledProcessError as exc:
