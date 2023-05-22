934c1aa7db012f1e90913edc8f37956d4f6b1a88
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-05-22 15:32:06 +0800

    [TECH-404] Perform deploy set linking for any type of k8s deploy
    branch: feature/TECH-404-link-up-deploy-set

--- src/mpyl/steps/deploy/kubernetes_job.py
+++ src/mpyl/steps/deploy/kubernetes_job.py
@@ -7,6 +7,8 @@ from .k8s.chart import ChartBuilder, to_cron_job_chart, to_job_chart
 from .. import Step, Meta
 from ..models import Input, Output, ArtifactType
 from ...project import Stage
+from ...stages.discovery import find_deploy_set
+from ...utilities.repo import RepoConfig
 
 
 class DeployKubernetesJob(Step):
@@ -20,6 +22,6 @@ class DeployKubernetesJob(Step):
         ), produced_artifact=ArtifactType.NONE, required_artifact=ArtifactType.DOCKER_IMAGE)
 
     def execute(self, step_input: Input) -> Output:
-        builder = ChartBuilder(step_input)
+        builder = ChartBuilder(step_input, find_deploy_set(RepoConfig(step_input.run_properties.config)))
         chart = to_cron_job_chart(builder) if builder.is_cron_job else to_job_chart(builder)
         return deploy_helm_chart(self._logger, chart, step_input, builder.release_name, delete_existing=True)
