8e976ed437a4acd693c3537275cb9eb690984fb8
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-05-31 17:20:14 +0800

    [TECH-296] Add version parameter
    and use current version by default
    
    branch: feature/TECH-296-local-dagster-run

--- src/mpyl/cli/build.py
+++ src/mpyl/cli/build.py
@@ -198,6 +198,12 @@ def ask_for_input(ctx, _param, value) -> Optional[str]:
     required=False
 )
 @click.option(
+    '--version', '-v',
+    help='A specific stable version on https://pypi.org/project/mpyl/ to use for the build. Default is the current.',
+    type=click.STRING,
+    required=False
+)
+@click.option(
     '--arguments', '-a',
     multiple=True,
     help='A series of arguments to pass to the pipeline. Note that will run within the pipenv in jenkins. '
@@ -221,14 +227,25 @@ def ask_for_input(ctx, _param, value) -> Optional[str]:
 )
 @click.option('--tag', '-tg', is_flag=False, flag_value="prompt", default="not_set", callback=ask_for_input)
 @click.pass_context
-def jenkins(ctx, user, password, pipeline, test, arguments, background, silent,  # pylint: disable=too-many-arguments
+def jenkins(ctx, user, password, pipeline, test, version, arguments, background, silent,  # pylint: disable=too-many-arguments
             tag):
     try:
+        if test and version:
+            raise click.BadArgumentUsage('Cannot specify both --test and --version')
+        if not version and not tag:
+            version = get_meta_version()
+
         upgrade_check = asyncio.wait_for(warn_if_update(ctx.obj.console), timeout=5)
         selected_pipeline = pipeline if pipeline else ctx.obj.config['jenkins']['defaultPipeline']
-        pipeline_parameters = {'TEST': 'true', 'VERSION': test} if test else {}
+
+        pipeline_parameters = {}
         if arguments:
             pipeline_parameters['PIPENV_PARAMS'] = " ".join(arguments)
+        if test:
+            pipeline_parameters['TEST_VERSION'] = test
+            pipeline_parameters['TEST'] = 'true'
+        elif version:
+            pipeline_parameters['VERSION'] = version
 
         run_argument = JenkinsRunParameters(
             jenkins_user=user, jenkins_password=password, config=ctx.obj.config,
