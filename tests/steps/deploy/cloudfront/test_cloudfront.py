f9994991a1a95393629fff4d99c56b7e41cf1fd0
Ben Lynch <benjaminlynch@vandebron.nl>
2023-06-06 16:23:42 +0200

    [TECH-128] refactoring based on PR comments (will squash commits before merge)
    branch: feature/TECH-128-cloudfront-kubernetes-deploy

--- tests/steps/deploy/cloudfront/test_cloudfront.py
+++ tests/steps/deploy/cloudfront/test_cloudfront.py
@@ -1,12 +1,20 @@
-from mpyl.steps.deploy.cloudfront_kubernetes_deploy import CloudFrontKubernetesDeploy
+import logging
+from unittest.mock import patch
+from mpyl.steps.deploy.cloudfront_kubernetes_deploy import CloudFrontKubernetesDeploy, STATIC_FOLDER
 from src.mpyl.steps.models import Input
 from tests.test_resources.test_data import get_cloudfront_project, RUN_PROPERTIES
 
 
 class TestCloudFront:
 
-    def test_get_bucket_name(self):
+    @patch('mpyl.steps.deploy.cloudfront_kubernetes_deploy.docker_copy')
+    @patch('mpyl.steps.deploy.cloudfront_kubernetes_deploy.docker_image_tag')
+    def test_copy_docker_assets_succeeds(self, mock_image_tag, mock_docker_copy):
+        mock_image_tag.return_value = 'test_image'
         project = get_cloudfront_project()
         step_input = Input(project, RUN_PROPERTIES, None)
-        bucket_name = CloudFrontKubernetesDeploy.get_bucket_name(step_input)
-        assert bucket_name == 'test-website-assets'
+
+        logger = logging.getLogger()
+        CloudFrontKubernetesDeploy.copy_docker_assets(logger, step_input=step_input, tmp_folder='tmp')
+
+        mock_docker_copy.assert_called_once()
--- tests/utilities/test_s3.py
+++ tests/utilities/test_s3.py
@@ -1,70 +1,30 @@
 from unittest.mock import patch
 import logging
-import pytest
-from botocore.exceptions import ClientError
-from mpyl.utilities.s3 import S3Client
+from mpyl.utilities.s3 import S3Client, S3ClientConfig
+from tests.test_resources import test_data
 
 
 class TestS3:
 
+    @staticmethod
+    def create_client_config() -> S3ClientConfig:
+        project = test_data.get_cloudfront_project()
+        run_properties = test_data.RUN_PROPERTIES
+        return S3ClientConfig(run_properties=run_properties, project=project)
+
     @patch('boto3.client')
     def test_create_client_succeeds(self, mock_boto3_client):
-        s3_config = {'s3': {'accessKeyId': 'testId', 'secretAccessKey': 'testKey'}}
-
-        S3Client(logger=logging.getLogger(), config=s3_config, bucket_name='test', root_path='root')
+        S3Client(logger=logging.getLogger(), config=TestS3.create_client_config())
         mock_boto3_client.assert_called_once()
 
-    def test_create_client_fails(self):
-        s3_config1 = {}
-        s3_config2 = {'s3': {}}
-        s3_config3 = {'s3': {'accessKeyId': 'testId'}}
-        s3_config4 = {'s3': {'secretAccessKey': 'testKey'}}
-        s3_config5 = {'s3': {'accessKeyId': ''}}
-        s3_config6 = {'s3': {'secretAccessKey': ''}}
-
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config1, bucket_name='test', root_path='root')
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config2, bucket_name='test', root_path='root')
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config3, bucket_name='test', root_path='root')
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config4, bucket_name='test', root_path='root')
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config5, bucket_name='test', root_path='root')
-        with pytest.raises(ValueError, match='S3 configuration not set'):
-            S3Client(logger=logging.getLogger(), config=s3_config6, bucket_name='test', root_path='root')
-
-    @patch('boto3.s3.inject.upload_file')
-    def test_upload_file_succeeds(self, mock_boto_upload_file):
-        s3_config = {'s3': {'accessKeyId': 'testId', 'secretAccessKey': 'testKey'}}
-        s3_client = S3Client(logger=logging.getLogger(), config=s3_config, bucket_name='test', root_path='root')
-
-        upload_successful = s3_client.upload_file(src_path='tests/test_resources/test_file.txt',
-                                                  dst_path='test_file.txt')
-
-        mock_boto_upload_file.assert_called_once_with('tests/test_resources/test_file.txt', 'test', 'test_file.txt')
-        assert upload_successful
-
-    @patch('boto3.s3.inject.upload_file')
-    def test_upload_file_fails(self, mock_boto_upload_file):
-        mock_boto_upload_file.side_effect = ClientError(operation_name='test', error_response={})
-
-        s3_config = {'s3': {'accessKeyId': 'testId', 'secretAccessKey': 'testKey'}}
-        s3_client = S3Client(logger=logging.getLogger(), config=s3_config, bucket_name='test', root_path='root')
-
-        upload_successful = s3_client.upload_file(src_path='tests/test_resources/test_file.txt',
-                                                  dst_path='test_file.txt')
-
-        mock_boto_upload_file.assert_called_once_with('tests/test_resources/test_file.txt', 'test', 'test_file.txt')
-        assert not upload_successful
-
     def test_create_dst_path_succeeds(self):
-        bucket_path1 = S3Client.create_file_dst(root_path='root', file_path='tmp', filename='filename.js')
-        bucket_path2 = S3Client.create_file_dst(root_path='root', file_path='tmp/js', filename='filename.js')
-        bucket_path3 = S3Client.create_file_dst(root_path='root', file_path='tmp/css', filename='filename.css')
-        bucket_path4 = S3Client.create_file_dst(root_path='root', file_path='tmp/assets', filename='filename.jpg')
-        bucket_path5 = S3Client.create_file_dst(root_path='root', file_path='tmp/assets/docs', filename='filename.pdf')
+        bucket_path1 = S3Client.create_file_dst(root_path='root', file_path='/tmp/asVx3sd/', filename='filename.js')
+        bucket_path2 = S3Client.create_file_dst(root_path='root', file_path='/tmp/asVx3sd/js', filename='filename.js')
+        bucket_path3 = S3Client.create_file_dst(root_path='root', file_path='/tmp/asVx3sd/css', filename='filename.css')
+        bucket_path4 = S3Client.create_file_dst(root_path='root', file_path='/tmp/asVx3sd/assets',
+                                                filename='filename.jpg')
+        bucket_path5 = S3Client.create_file_dst(root_path='root', file_path='/tmp/asVx3sd/assets/docs',
+                                                filename='filename.pdf')
 
         assert bucket_path1 == 'root/filename.js'
         assert bucket_path2 == 'root/js/filename.js'
