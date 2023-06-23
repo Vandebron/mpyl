3ae63163c7ebd1f1c84debd9b7c4b9500a292be5
katrin <katringrunert@vandebron.nl>
2023-06-23 15:02:01 +0200

    [TECH-116] Update name and description of DeployDagster Step
    branch: feature/TECH-116-dagster-deploy-step

--- src/mpyl/steps/deploy/dagster.py
+++ src/mpyl/steps/deploy/dagster.py
@@ -12,8 +12,8 @@ class DeployDagster(Step):
 
     def __init__(self, logger: Logger) -> None:
         super().__init__(logger, Meta(
-            name='Kubernetes Job Deploy',
-            description='Deploy a job to k8s',
+            name='Dagster Deploy',
+            description='Deploy a dagster user code repository to k8s',
             version='0.0.1',
             stage=Stage.DEPLOY
         ), produced_artifact=ArtifactType.NONE, required_artifact=ArtifactType.DOCKER_IMAGE)
