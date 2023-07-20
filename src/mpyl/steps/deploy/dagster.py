075f0e26dfde5b5563b7661e9233d94b9a30b960
katrin <katringrunert@vandebron.nl>
2023-07-20 16:12:20 +0200

    [TECH-116] Use correct deployments reference for grpc-server creation
    branch: feature/TECH-116-dagster-deploy-step

--- src/mpyl/steps/deploy/dagster.py
+++ src/mpyl/steps/deploy/dagster.py
@@ -81,9 +81,9 @@ class DeployDagster(Step):
             new_workspace_servers_list = dagster_workspace["load_from"]
             new_workspace_servers_list.append(
                 to_grpc_server_entry(
-                    host=user_code_deployment["name"],
-                    port=user_code_deployment["port"],
-                    name=user_code_deployment["name"],
+                    host=user_code_deployment["deployments"][0]["name"],
+                    port=user_code_deployment["deployments"][0]["port"],
+                    name=user_code_deployment["deployments"][0]["name"],
                 )
             )
             self._logger.info(dagster_workspace)
