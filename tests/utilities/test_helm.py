79d8577c7db001b127281e86797d02ae036e3161
katrin <katringrunert@vandebron.nl>
2023-07-21 15:37:20 +0200

    [TECH-116] Formatting
    branch: feature/TECH-116-dagster-deploy-step

--- tests/utilities/test_helm.py
+++ tests/utilities/test_helm.py
@@ -2,7 +2,8 @@ from mpyl.utilities.helm import convert_name_to_helm_release_name
 
 
 class TestHelm:
-
     def test_convert_to_helm_release_name(self):
-        assert convert_name_to_helm_release_name("my-project", "-pr-1234") == "mp-pr-1234"
+        assert (
+            convert_name_to_helm_release_name("my-project", "-pr-1234") == "mp-pr-1234"
+        )
         assert convert_name_to_helm_release_name("my-project", "") == "mp"
