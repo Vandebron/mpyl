8ad2a80447740e0c99f7479cfcb865258eb0a287
koeves <daniel.koves@icloud.com>
2023-07-26 14:55:15 +0200

    formatting

--- src/mpyl/steps/deploy/k8s/chart.py
+++ src/mpyl/steps/deploy/k8s/chart.py
@@ -515,13 +515,16 @@ class ChartBuilder:  # pylint: disable = too-many-instance-attributes
             for e in self.env
             if e.get_value(self.target) is not None
         }
+        pr_identifier = (
+            self.step_input.run_properties.versioning.pr_number
+            if not self.step_input.run_properties.versioning.tag
+            else None
+        )
         substituted = substitute_namespaces(
             raw_env_vars,
             {p.to_name for p in self.deploy_set.all_projects},
             {p.to_name for p in self.deploy_set.projects_to_deploy},
-            pr_identifier=self.step_input.run_properties.versioning.pr_number
-            if not self.step_input.run_properties.versioning.tag
-            else None,
+            pr_identifier,
         )
 
         env_vars = [
