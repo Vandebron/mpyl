81b006309b779c3e675e8ef3d4fe31d3e61bb8b0
ewaldschrader <ewaldschrader@vandebron.nl>
2023-08-24 15:56:12 +0200

    [TECH-435] debug
    branch: feature/TECH-435-JENKINS-DRYRUN

--- src/mpyl/cli/__init__.py
+++ src/mpyl/cli/__init__.py
@@ -31,7 +31,7 @@ class CliContext:
     run_properties: dict
 
 
-@dataclass(frozen=True) # type: ignore
+@dataclass(frozen=True)  # type: ignore
 class MpylCliParameters:
     local: bool = False
     tag: Optional[str] = None
