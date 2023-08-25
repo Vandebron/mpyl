c828abbe3dffded336dc720527bb91cc8f3e05a7
ewaldschrader <ewaldschrader@vandebron.nl>
2023-08-25 11:42:52 +0200

    [TECH-435] debug
    branch: feature/TECH-435-JENKINS-DRYRUN

--- src/mpyl/cli/__init__.py
+++ src/mpyl/cli/__init__.py
@@ -38,7 +38,6 @@ class MpylCliParameters:
     pull_main: bool = False
     verbose: bool = False
     all: bool = False
-    dryrun: bool = False
 
 
 async def fetch_latest_version() -> Optional[str]:
