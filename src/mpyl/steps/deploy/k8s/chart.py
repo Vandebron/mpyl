410eefaeabfad3414bdeff0c64701800eaf49af8
Jorg88 <jorgpost@vandebron.nl>
2023-08-29 11:24:56 +0200

    [TECH-548] Fallback to the default port mapping instead of the first host serviceport value
    branch: feature/TECH-548-fix-multiple-port-mapping

--- src/mpyl/steps/deploy/k8s/chart.py
+++ src/mpyl/steps/deploy/k8s/chart.py
@@ -380,13 +380,6 @@ class ChartBuilder:  # pylint: disable = too-many-instance-attributes
             self.deployment.traefik.hosts if self.deployment.traefik else []
         )
 
-        first_host = next(iter(hosts), None)
-        default_service_port = (
-            first_host.service_port
-            if first_host and first_host.service_port
-            else self.__find_default_port()
-        )
-
         configured_addresses = self.config_defaults.white_lists["addresses"]
         address_dictionary = {
             address["name"]: address["values"] for address in configured_addresses
@@ -410,7 +403,7 @@ class ChartBuilder:  # pylint: disable = too-many-instance-attributes
                 index=idx,
                 service_port=host.service_port
                 if host.service_port
-                else default_service_port,
+                else self.__find_default_port(),
                 white_lists=to_white_list(host.whitelists),
             )
             for idx, host in enumerate(hosts if hosts else default_hosts)
