20ee5fd3c056de8ad781db3f0ec02123bd09f310
ewaldschrader <ewaldschrader@vandebron.nl>
2023-09-07 20:57:57 +0200

    [TECH-435] debug
    branch: feature/TECH-435-CLI-VERSION

--- src/mpyl/cli/build.py
+++ src/mpyl/cli/build.py
@@ -427,10 +427,8 @@ def jenkins(  # pylint: disable=too-many-locals, too-many-arguments
     dryrun_,
     version,
 ):
-    if version:
-        os.environ["MPYL_RELEASE"] = version
     upgrade_check = None
-
+    version = version
     try:
         upgrade_check = asyncio.wait_for(warn_if_update(ctx.obj.console), timeout=5)
         if "jenkins" not in ctx.obj.config:
