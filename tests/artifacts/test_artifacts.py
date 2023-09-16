593cc372e8baf496a3fab2bff088af38313d4aa3
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-09-16 12:45:14 +0800

    [TECH-521] Add target path within the repository
    branch: feature/TECH-521-simplified-remote-artifact-caching

--- tests/artifacts/test_artifacts.py
+++ tests/artifacts/test_artifacts.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from pathlib import Path
 
 from src.mpyl.artifacts.build_artifacts import ArtifactsRepository
 from src.mpyl.utilities.repo import RepoConfig
@@ -14,23 +16,22 @@ class TestArtifacts:
 
     def test_clone_to_temp_dir(self):
         repository = get_repo()
-        artifacts = ArtifactsRepository(
-            logger=logging.getLogger(), codebase_repo=repository
-        )
         artifact_repo_config = RepoConfig(
             main_branch="main",
             ignore_patterns=[],
             repo_credentials=RepoCredentials(
-                url="https://<github_token>@github.com/SamTheisens/mpyl-example-argocd.git",
+                url=f"https://{os.environ.get('GITHUB_CREDS')}@github.com/SamTheisens/mpyl-example-argocd.git",
                 ssh_url="git@github.com:Vandebron/mpyl-artifacts.git",
                 user_name="",
                 password="",
             ),
         )
+        artifacts = ArtifactsRepository(
+            logger=logging.getLogger(),
+            codebase_repo=repository,
+            artifact_repo_config=artifact_repo_config,
+            path_within_artifact_repo=Path("mpyl-cache"),
+        )
 
-        artifacts.pull(
-            artifact_repo_config=artifact_repo_config, branch="test/build-artifacts"
-        )
-        artifacts.push(
-            artifact_repo_config=artifact_repo_config, branch="test/build-artifacts"
-        )
+        # artifacts.pull(branch="test/build-artifacts")
+        artifacts.push(branch="test/build-artifacts", file_paths=[])
