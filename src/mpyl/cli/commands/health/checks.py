427c6e7b76c1cedce31ff0aa740b1e509a8e3c8c
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-09-24 10:43:51 +0700

    [TECH-491] Extract path validation logic
    to separate concerns
    
    branch: feature/TECH-491-rename-cvs-to-vcs

--- src/mpyl/cli/commands/health/checks.py
+++ src/mpyl/cli/commands/health/checks.py
@@ -7,6 +7,7 @@ import shutil
 import sys
 from pathlib import Path
 from subprocess import CalledProcessError
+from typing import Optional
 
 import jsonschema
 from dotenv import load_dotenv
@@ -15,6 +16,7 @@ from rich.markdown import Markdown
 from rich.prompt import Confirm
 
 from ..build.jenkins import get_token
+from ....cli import get_latest_publication, get_meta_version
 from ....constants import DEFAULT_CONFIG_FILE_NAME, DEFAULT_RUN_PROPERTIES_FILE_NAME
 from ....projects.versioning import (
     check_upgrade_needed,
@@ -26,7 +28,6 @@ from ....projects.versioning import (
 from ....utilities.github import GithubConfig
 from ....utilities.jenkins import JenkinsConfig
 from ....utilities.pyaml_env import parse_config
-from ....cli import get_latest_publication, get_meta_version
 from ....validation import validate
 
 
@@ -53,24 +54,32 @@ def perform_health_checks(bare_console: Console, is_ci: bool = False):
     __check_version(console)
 
     console.title("MPyL configuration")
-    __check_config(
+    if config_path := __validate_config_path(
         console,
         env_var="MPYL_CONFIG_PATH",
         default=DEFAULT_CONFIG_FILE_NAME,
-        schema_path="../../../schema/mpyl_config.schema.yml",
-        name="config",
-        upgraders=CONFIG_UPGRADERS,
-    )
+        config_name="config",
+    ):
+        __validate_config(
+            console,
+            config_file_path=config_path,
+            schema_path="../../../schema/mpyl_config.schema.yml",
+            upgraders=CONFIG_UPGRADERS,
+        )
 
     console.title("Run configuration")
-    __check_config(
+    if properties_path := __validate_config_path(
         console,
         env_var="MPYL_RUN_PROPERTIES_PATH",
         default=DEFAULT_RUN_PROPERTIES_FILE_NAME,
-        schema_path="../../../schema/run_properties.schema.yml",
-        name="run properties",
-        upgraders=[],
-    )
+        config_name="run properties",
+    ):
+        __validate_config(
+            console,
+            config_file_path=properties_path,
+            schema_path="../../../schema/run_properties.schema.yml",
+            upgraders=[],
+        )
 
     if not is_ci:
         console.title("Jenkins")
@@ -153,60 +162,64 @@ def __check_version(console):
         console.check("Could not determine latest version", success=False)
 
 
-def __check_config(
-    console: HealthConsole,
-    env_var: str,
-    default: str,
-    schema_path: str,
-    name: str,
-    upgraders: list[Upgrader],
-):
+def __validate_config_path(
+    console: HealthConsole, env_var: str, default: str, config_name: str
+) -> Optional[Path]:
     path_env = os.environ.get(env_var)
     path = Path(path_env or default)
     location = (
-        f"{name} at `{path}` via environment variable `{env_var}`"
+        f"{config_name} at `{path}` via environment variable `{env_var}`"
         if path_env
-        else f"{name} at `{path}`"
+        else f"{config_name} at `{path}`"
     )
     if os.path.exists(path):
         console.check(f"Found {location}", success=True)
+        return path
 
-        path, diff = check_upgrade_needed(path, upgraders)
-        pretty_diff = pretty_print(diff) if diff else ""
-        if pretty_diff == "":
-            console.check("Upgrade not necessary", success=True)
-        else:
-            console.check("Upgrade required", success=False)
-            console.print("Expected changes:")
-            console.print(pretty_diff)
-            if sys.stdout.isatty() and Confirm.ask("Upgrade now?"):
-                upgraded = upgrade_file(path, upgraders)
-                if upgraded:
-                    path.write_text(upgraded, encoding="utf-8")
-                    console.check(
-                        "Upgrade successful. You may need to run `mpyl projects upgrade` still.",
-                        success=True,
-                    )
-                else:
-                    console.check("Could not upgrade", success=False)
+    console.check(
+        f"Could not find {location}. Location can be specified with env var '{env_var}'",
+        success=False,
+    )
+    return None
 
-        if load_dotenv(Path(".env")):
-            console.check("Set env variables via .env file", success=True)
 
-        parsed = parse_config(path)
-        schema_dict = pkgutil.get_data(__name__, schema_path)
-        if schema_dict:
-            try:
-                validate(parsed, schema_dict.decode("utf-8"))
-                console.check(f"{name.capitalize()} is valid", success=True)
-            except jsonschema.exceptions.ValidationError as exc:
-                console.check(
-                    f"{name.capitalize()} is invalid: {exc.message} at '{'.'.join(map(str, exc.path))}'."
-                    f" 🤔 Did you rebase your branch onto {parsed.get('vcs', {}).get('git', {}).get('mainBranch')}?",
-                    success=False,
-                )
+def __validate_config(
+    console: HealthConsole,
+    config_file_path: Path,
+    schema_path: str,
+    upgraders: list[Upgrader],
+):
+    path, diff = check_upgrade_needed(config_file_path, upgraders)
+    pretty_diff = pretty_print(diff) if diff else ""
+    if pretty_diff == "":
+        console.check("Upgrade not necessary", success=True)
     else:
-        console.check(
-            f"Could not find {location}. Location can be specified with env var '{env_var}'",
-            success=False,
-        )
+        console.check("Upgrade required", success=False)
+        console.print("Expected changes:")
+        console.print(pretty_diff)
+        if sys.stdout.isatty() and Confirm.ask("Upgrade now?"):
+            upgraded = upgrade_file(path, upgraders)
+            if upgraded:
+                path.write_text(upgraded, encoding="utf-8")
+                console.check(
+                    "Upgrade successful. You may need to run `mpyl projects upgrade` still.",
+                    success=True,
+                )
+            else:
+                console.check("Could not upgrade", success=False)
+
+    if load_dotenv(Path(".env")):
+        console.check("Set env variables via .env file", success=True)
+
+    parsed = parse_config(path)
+    schema_dict = pkgutil.get_data(__name__, schema_path)
+    if schema_dict:
+        try:
+            validate(parsed, schema_dict.decode("utf-8"))
+            console.check(f"{config_file_path} is valid", success=True)
+        except jsonschema.exceptions.ValidationError as exc:
+            console.check(
+                f"{config_file_path} is invalid: {exc.message} at '{'.'.join(map(str, exc.path))}'."
+                f" 🤔 Did you rebase your branch onto {parsed.get('vcs', {}).get('git', {}).get('mainBranch')}?",
+                success=False,
+            )
