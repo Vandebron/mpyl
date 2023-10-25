3e0d6120289cbad7621efa6b7aea3032abc8d2b1
Nik Kashi <mhmhkashi@gmail.com>
2023-10-25 15:18:46 +0200

    [TECH-494] Open repo as a resource

--- tests/projects/test_load.py
+++ tests/projects/test_load.py
@@ -5,18 +5,18 @@ from tests.test_resources import test_data
 
 class TestProjectLoad:
     def test_load_all_projects(self):
-        repo = test_data.get_repo()
-        projects = load_projects(repo.root_dir, repo.find_projects(), True)
-        assert len(projects) == 11
+        with test_data.get_repo() as repo:
+            projects = load_projects(repo.root_dir, repo.find_projects(), True)
+            assert len(projects) == 11
 
     def test_load_all_project_dependencies(self):
-        repo = test_data.get_repo()
-        projects = load_projects(repo.root_dir, repo.find_projects())
-        dependencies = list(map(lambda p: find_dependencies(p, projects), projects))
-        deps: dict[str, ProjectWithDependents] = dict(
-            map(lambda d: (d.name, d), dependencies)
-        )
+        with test_data.get_repo() as repo:
+            projects = load_projects(repo.root_dir, repo.find_projects())
+            dependencies = list(map(lambda p: find_dependencies(p, projects), projects))
+            deps: dict[str, ProjectWithDependents] = dict(
+                map(lambda d: (d.name, d), dependencies)
+            )
 
-        assert len(dependencies) == 11
-        assert len(deps["job"].dependent_projects) == 1
-        assert len(deps["sbtservice"].dependent_projects) == 0
+            assert len(dependencies) == 11
+            assert len(deps["job"].dependent_projects) == 1
+            assert len(deps["sbtservice"].dependent_projects) == 0
