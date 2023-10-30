e460e8ab668094e51bb872f9b844e7ca83518c4b
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2023-10-30 18:30:03 +0800

    [TECH-293] Explain that there's nothing to do if the execution plan is empty
    branch: feature/TECH-293-flexible-stage-definitions

--- src/mpyl/reporting/formatting/markdown.py
+++ src/mpyl/reporting/formatting/markdown.py
@@ -108,6 +108,8 @@ def execution_plan_as_markdown(run_result: RunResult):
         result += f"\n\n{run_result.failed_result.output.message}\n\n"
     for stage in run_result.run_properties.stages:
         result += markdown_for_stage(run_result, stage)
+    if result == "":
+        return "🤷 Nothing to do"
     return result
 
 
