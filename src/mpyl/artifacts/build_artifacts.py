8b56f3de38c61b1126fe3dbf01843dbc4297d112
Jorg88 <jorgpost@vandebron.nl>
2024-01-31 18:04:34 +0100

    Add some logging around the caching push logic

--- src/mpyl/artifacts/build_artifacts.py
+++ src/mpyl/artifacts/build_artifacts.py
@@ -139,12 +139,14 @@ class ArtifactsRepository:
                 artifact_repo.commit(message)
 
                 try:  # to prevent issues with parallel runs pushing to the same branch
+                    artifact_repo.pull()
+                    self.logger.info("Pushing changes to remote")
                     artifact_repo.push(branch)
                 except GitCommandError:
-                    if remote_branch_exists:
-                        time.sleep(1)
-                        artifact_repo.pull()
-                        artifact_repo.push(branch)
+                    self.logger.info("Retrying push after pulling from remote..")
+                    time.sleep(1)
+                    artifact_repo.pull()
+                    artifact_repo.push(branch)
 
                 self.logger.info(
                     f"Pushed {branch} with {copied_paths} copied paths to {artifact_repo.remote_url}"
