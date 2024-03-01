1bf0d8c6460bd8f7a3507fb4a34069e9707416dc
Jo Liu <yazhouliu@vandebron.nl>
2024-03-01 22:22:32 +0100

    change code style

--- src/mpyl/steps/deploy/bpmn.py
+++ src/mpyl/steps/deploy/bpmn.py
@@ -21,15 +21,16 @@ class BpmnDiagramDeploy(Step):
             ),
             produced_artifact=ArtifactType.NONE,
             required_artifact=ArtifactType.NONE,
-            # TO DO: make an artifcatype with a camunda deployed diagram link -> miss information to construct dynamic link...
+            # TO DO: make an artifcatype with a camunda deployed diagram link
         )
 
     def execute(self, step_input: Input) -> Output:
-        volume_path = os.path.join(os.getcwd(), 'bpm')
-        camunda_config = CamundaConfig.from_config(step_input.run_properties.config, step_input.run_properties.target)
+        volume_path = os.path.join(os.getcwd(), "bpm")
+        camunda_config = CamundaConfig.from_config(
+            step_input.run_properties.config, step_input.run_properties.target
+        )
         docker_container = self._get_docker_container(volume_path, camunda_config)
-        bpm_file_path = volume_path+'/camunda-diagrams/src/test/resources/'
-        
+        bpm_file_path = volume_path + "/camunda-diagrams/src/test/resources/"
         try:
             for file_name in (
                 [fn for fn in os.listdir(bpm_file_path) if fn.endswith(".bpmn")]
@@ -38,17 +39,19 @@ class BpmnDiagramDeploy(Step):
             ):
                 self._logger.info(file_name)
                 run_command = (
-                    f'zbctl deploy camunda-diagrams/src/test/resources/{file_name}'
+                    f"zbctl deploy camunda-diagrams/src/test/resources/{file_name}"
                 )
                 result = execute_with_stream(
                     logger=self._logger,
                     container=docker_container,
                     command=run_command,
-                    task_name=f'Deploy bpmn diagram {file_name}',
+                    task_name=f"Deploy bpmn diagram {file_name}",
                 )
                 for stdout in result:
                     if "error Command failed with exit code" in stdout:
-                        raise DockerException(command_launched=[run_command], return_code=1)
+                        raise DockerException(
+                            command_launched=[run_command], return_code=1
+                        )
         except DockerException:
             return Output(
                 success=False,
@@ -64,7 +67,7 @@ class BpmnDiagramDeploy(Step):
             message=f"Deployed all diagrams in {step_input.project.name}",
             produced_artifact=None,
         )
-        
+
     def _get_docker_container(
         self, volume_path: str, camunda_config: CamundaConfig
     ) -> Container:
@@ -80,12 +83,12 @@ class BpmnDiagramDeploy(Step):
             image=custom_image_tag,
             interactive=True,
             detach=True,
-            volumes=[
-                (volume_path,"/camunda-diagrams")
-            ],
-            envs={"ZEEBE_ADDRESS": camunda_config.cluster_id,
-                  "ZEEBE_CLIENT_ID": camunda_config.client_id,
-                  "ZEEBE_CLIENT_SECRET":camunda_config.client_secret},
+            volumes=[(volume_path, "/camunda-diagrams")],
+            envs={
+                "ZEEBE_ADDRESS": camunda_config.cluster_id,
+                "ZEEBE_CLIENT_ID": camunda_config.client_id,
+                "ZEEBE_CLIENT_SECRET": camunda_config.client_secret,
+            },
             workdir="/camunda-diagrams",
         )
         if not isinstance(docker_container, Container):
