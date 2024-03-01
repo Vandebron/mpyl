7a06bf5b1edf094de7675a681fd4d6b0ea2243e4
Jo Liu <yazhouliu@vandebron.nl>
2024-03-01 22:22:32 +0100

    change code style

--- src/mpyl/utilities/bpmn/__init__.py
+++ src/mpyl/utilities/bpmn/__init__.py
@@ -11,20 +11,17 @@ from ...project import Target, TargetProperty
 class CamundaConfig:
     cluster_id: str
     client_id: str
-    client_secret:str
-    
+    client_secret: str
+
     @staticmethod
     def from_config(config: dict, target: Target):
-        camunda_config: dict = config.get("camunda")
+        camunda_config = config.get("camunda")
         if not camunda_config:
             raise KeyError("Camunda section needs to be defined in mpyl_config.yml")
-        
-        config = TargetProperty.from_config(camunda_config).get_value(target)
 
+        config = TargetProperty.from_config(camunda_config).get_value(target)
         return CamundaConfig(
-            cluster_id=config.get("clusterId"),
-            client_id=config.get("clientId"),
-            client_secret=config.get("clientSecret")
+            cluster_id=str(config.get("clusterId")),
+            client_id=str(config.get("clientId")),
+            client_secret=str(config.get("clientSecret")),
         )
-        
-        
