3ceed2d55ac9818e53be6cf0fa116707f41164cf
Pedro Taborda <pedrotaborda@vandebron.nl>
2024-03-19 20:58:02 +0100

    fixed linting

--- src/mpyl/artifacts/build_artifacts.py
+++ src/mpyl/artifacts/build_artifacts.py
@@ -129,7 +129,9 @@ class ArtifactsRepository:
                 artifact_repo.checkout_branch(branch_name=branch)
                 path_in_repo = repo_path / self.path_within_artifact_repo
                 self.logger.info(f"repo_path: '{repo_path}'")
-                self.logger.info(f"path_within_artifact_repo: '{self.path_within_artifact_repo}'")
+                self.logger.info(
+                    f"path_within_artifact_repo: '{self.path_within_artifact_repo}'"
+                )
                 self.logger.info(f"path in repo: '{path_in_repo}'")
                 g = f"{str(path_in_repo)}/*"
                 self.logger.info(f"path in repo: '{glob.glob(g)}'")
