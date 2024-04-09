cf13385e41a19125078f71ddd7ecb9ccbe915244
Jorg88 <jorgpost@vandebron.nl>
2024-04-09 09:41:12 +0200

    Generate a unique run result file per process

--- mpyl-reporter.py
+++ mpyl-reporter.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from rich.console import Console
 from rich.logging import RichHandler
 
-from mpyl.constants import BUILD_ARTIFACTS_FOLDER
+from mpyl.constants import BUILD_ARTIFACTS_FOLDER, RUN_RESULT_FILE_GLOB
 from mpyl.reporting.targets import ReportAccumulator
 from mpyl.reporting.targets.github import CommitCheck
 from mpyl.reporting.targets.github import PullRequestReporter
@@ -20,55 +20,60 @@ from mpyl.utilities.pyaml_env import parse_config
 
 
 def main(logger: Logger):
-    run_result_file = Path(BUILD_ARTIFACTS_FOLDER) / "run_result"
-
-    if not run_result_file.is_file():
-        logger.info(f"Run result file {run_result_file} not found. Nothing to report.")
+    run_result_files = list(Path(BUILD_ARTIFACTS_FOLDER).glob(RUN_RESULT_FILE_GLOB))
+    if len(run_result_files) == 0:
+        logger.info(
+            f"Run result file(s) {RUN_RESULT_FILE_GLOB} not found. Nothing to report."
+        )
         sys.exit()
 
-    with open(run_result_file, "rb") as file:
-        run_result: RunResult = pickle.load(file)
+    config = parse_config("mpyl_config.yml")
+    properties = parse_config("run_properties.yml")
+    run_properties = construct_run_properties(config=config, properties=properties)
+    run_result: RunResult = RunResult(run_properties=run_properties)
+    for run_result_file in run_result_files:
+        with open(run_result_file, "rb") as file:
+            previous_result: RunResult = pickle.load(file)
+            run_result.update_run_plan(previous_result.run_plan)
+            run_result.extend(previous_result.results)
 
-        accumulator = ReportAccumulator()
-        config = parse_config("mpyl_config.yml")
-        properties = parse_config("run_properties.yml")
-        run_properties = construct_run_properties(config=config, properties=properties)
+    accumulator = ReportAccumulator()
 
-        commit_check = CommitCheck(config=config, logger=logger)
-        accumulator.add(commit_check.send_report(run_result))
+    commit_check = CommitCheck(config=config, logger=logger)
+    accumulator.add(commit_check.send_report(run_result))
 
-        slack_channel = SlackReporter(
+    slack_channel = SlackReporter(
+        config=config,
+        channel="#project-mpyl-notifications",
+        versioning_identifier=run_properties.versioning.identifier,
+        target=run_properties.target,
+    )
+    accumulator.add(slack_channel.send_report(run_result))
+
+    if run_properties.details.user_email:
+        slack_personal = SlackReporter(
             config=config,
-            channel="#project-mpyl-notifications",
+            channel=None,
             versioning_identifier=run_properties.versioning.identifier,
             target=run_properties.target,
         )
-        accumulator.add(slack_channel.send_report(run_result))
+        slack_personal.send_report(run_result)
 
-        if run_properties.details.user_email:
-            slack_personal = SlackReporter(
-                config=config,
-                channel=None,
-                versioning_identifier=run_properties.versioning.identifier,
-                target=run_properties.target,
-            )
-            slack_personal.send_report(run_result)
+    jira = JiraReporter(
+        config=config, branch=run_properties.versioning.branch, logger=logger
+    )
+    accumulator.add(jira.send_report(run_result))
 
-        jira = JiraReporter(
-            config=config, branch=run_properties.versioning.branch, logger=logger
+    github_comment = PullRequestReporter(
+        config=config,
+        compose_function=compose_build_status,
+    )
+    accumulator.add(github_comment.send_report(run_result))
+
+    if accumulator.failures:
+        logger.warning(
+            f'Failed to send the following report(s): {", ".join(accumulator.failures)}'
         )
-        accumulator.add(jira.send_report(run_result))
-
-        github_comment = PullRequestReporter(
-            config=config,
-            compose_function=compose_build_status,
-        )
-        accumulator.add(github_comment.send_report(run_result))
-
-        if accumulator.failures:
-            logger.warning(
-                f'Failed to send the following report(s): {", ".join(accumulator.failures)}'
-            )
 
 
 if __name__ == "__main__":
--- src/mpyl/constants.py
+++ src/mpyl/constants.py
@@ -6,3 +6,5 @@ DEFAULT_RUN_PROPERTIES_FILE_NAME = "run_properties.yml"
 DEFAULT_STAGES_SCHEMA_FILE_NAME = "mpyl_stages.schema.yml"
 
 PR_NUMBER_PLACEHOLDER = "{PR-NUMBER}"
+
+RUN_RESULT_FILE_GLOB = "run_result-*.pickle"
